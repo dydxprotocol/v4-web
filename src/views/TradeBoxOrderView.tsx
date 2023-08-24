0e26433e29f6cad54cc818e46f48e6a087167a66
jaredvu <jaredvu@gmail.com>
2023-08-24 14:18:55 -0700

    disable conditional orders on testnet until they support them

--- src/views/TradeBoxOrderView.tsx
+++ src/views/TradeBoxOrderView.tsx
@@ -47,6 +47,7 @@ const useTradeTypeOptions = () => {
                   value && {
                     value: value as TradeTypes,
                     label,
+                    disabled: import.meta.env.MODE === 'production', // TODO: Remove this when stop orders are supported on testnet
                   }
               )
               .filter(isTruthy),
