80be9b357f23a058cf97af6eb0cf093afa871ca7
jaredvu <jaredvu@gmail.com>
2023-08-28 21:18:30 -0700

    remove console.log

--- src/lib/abacus/filesystem.ts
+++ src/lib/abacus/filesystem.ts
@@ -3,7 +3,6 @@ import { DEV_ENDPOINTS, ENDPOINTS } from '@dydxprotocol/v4-localization';
 
 class AbacusFileSystem implements AbacusFileSystemProtocol {
   readTextFile(location: FileLocation, path: string): Nullable<string> {
-    console.log({ location, path });
     if (path === 'config/staging/environments.json') {
       const environments = import.meta.env.MODE === 'production' ? ENDPOINTS : DEV_ENDPOINTS;
       return JSON.stringify(environments);
