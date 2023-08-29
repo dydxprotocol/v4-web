271c98c9bb0f6a6cf9b9b80caf9eabbd41fd77f5
jaredvu <jaredvu@gmail.com>
2023-08-29 09:43:26 -0700

    use NETWORK_ENDPOINTS const

--- src/lib/abacus/filesystem.ts
+++ src/lib/abacus/filesystem.ts
@@ -1,6 +1,5 @@
-import { DEV_ENDPOINTS, ENDPOINTS } from '@dydxprotocol/v4-localization';
-
 import type { AbacusFileSystemProtocol, FileLocation, Nullable } from '@/constants/abacus';
+import { NETWORK_ENDPOINTS } from '@/constants/networks';
 
 export const ENDPOINTS_PATH =
   import.meta.env.MODE === 'production'
@@ -10,8 +9,7 @@ export const ENDPOINTS_PATH =
 class AbacusFileSystem implements AbacusFileSystemProtocol {
   readTextFile(location: FileLocation, path: string): Nullable<string> {
     if (path === ENDPOINTS_PATH) {
-      const environments = import.meta.env.MODE === 'production' ? ENDPOINTS : DEV_ENDPOINTS;
-      return JSON.stringify(environments);
+      return JSON.stringify(NETWORK_ENDPOINTS);
     }
 
     return null;
