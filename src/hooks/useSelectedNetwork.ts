0abe96915d534f978c8a0787831f966d9ad77380
Aleka Cheung <aforaleka@gmail.com>
2023-09-15 16:55:42 -0400

    remove offline exchange dialog from testnet3 prod

--- src/hooks/useSelectedNetwork.ts
+++ src/hooks/useSelectedNetwork.ts
@@ -1,15 +1,13 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { DialogTypes } from '@/constants/dialogs';
 import { LocalStorageKey } from '@/constants/localStorage';
-import { CLIENT_NETWORK_CONFIGS, DEFAULT_APP_ENVIRONMENT, DydxNetwork } from '@/constants/networks';
+import { DEFAULT_APP_ENVIRONMENT, DydxNetwork } from '@/constants/networks';
 
 import { useAccounts, useLocalStorage } from '@/hooks';
 
 import { setSelectedNetwork } from '@/state/app';
 import { getSelectedNetwork } from '@/state/appSelectors';
-import { openDialog } from '@/state/dialogs';
 
 export const useSelectedNetwork = (): {
   switchNetwork: (network: DydxNetwork) => void;
@@ -23,17 +21,6 @@ export const useSelectedNetwork = (): {
     key: LocalStorageKey.SelectedNetwork,
     defaultValue: DEFAULT_APP_ENVIRONMENT,
   });
-
-  useEffect(() => {
-    if (
-      import.meta.env.MODE === 'production' &&
-      CLIENT_NETWORK_CONFIGS[selectedNetwork].dydxChainId === 'dydx-testnet-3'
-    ) {
-      dispatch(
-        openDialog({ type: DialogTypes.ExchangeOffline, dialogProps: { preventClose: true } })
-      );
-    }
-  }, [selectedNetwork]);
 
   const switchNetwork = useCallback(
     (network: DydxNetwork) => {
