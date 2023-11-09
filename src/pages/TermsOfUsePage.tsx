4513a1f379fdb925d5641840470a7c7bd7a429d5
Aleka Cheung <aforaleka@gmail.com>
2023-11-09 15:07:44 -0500

    add back article mixin

--- src/pages/TermsOfUsePage.tsx
+++ src/pages/TermsOfUsePage.tsx
@@ -1,5 +1,7 @@
 import styled, { AnyStyledComponent } from 'styled-components';
 
+import { articleMixins } from '@/styles/articleMixins';
+
 export const TermsOfUsePage = () => (
   <Styled.Article>
     <header>
@@ -1077,30 +1079,5 @@ export const TermsOfUsePage = () => (
 const Styled: Record<string, AnyStyledComponent> = {};
 
 Styled.Article = styled.article`
-  padding: 2rem;
-
-  overflow-wrap: break-word;
-  user-select: text;
-
-  margin: 0 auto;
-  width: 60rem;
-  max-width: 100vw;
-
-  header {
-    color: var(--color-text-0);
-    margin-bottom: 2rem;
-
-    h1 {
-      font: var(--font-extra-book);
-      color: var(--color-text-2);
-    }
-  }
-
-  p {
-    padding: 0.5rem 0;
-  }
-
-  ul {
-    padding: 0.5rem 1rem;
-  }
+  ${articleMixins.article}
 `;
