378a2d87258721a75eab8620eca298cca198c321
jaredvu <jaredvu@gmail.com>
2023-11-20 14:14:40 -0800

    Button -> Toggle button, add offset for notifs

--- src/layout/NotificationsToastArea.tsx
+++ src/layout/NotificationsToastArea.tsx
@@ -2,7 +2,7 @@ import { useMemo, useState } from 'react';
 import styled, { css } from 'styled-components';
 
 import { NotificationStatus } from '@/constants/notifications';
-import { ButtonAction, ButtonShape, ButtonSize } from '@/constants/buttons';
+import { ButtonShape, ButtonSize } from '@/constants/buttons';
 import { useNotifications } from '@/hooks/useNotifications';
 import { useBreakpoints } from '@/hooks/useBreakpoints';
 import { ChevronLeftIcon } from '@/icons';
@@ -11,6 +11,7 @@ import { breakpoints } from '@/styles';
 import { Button } from '@/components/Button';
 import { Toast } from '@/components/Toast';
 import { ToastArea } from '@/components/ToastArea';
+import { ToggleButton } from '@/components/ToggleButton';
 
 type StyleProps = {
   className?: string;
@@ -46,36 +47,37 @@ export const NotificationsToastArea = ({ className }: StyleProps) => {
           key: getKey(notification),
           displayData: getDisplayData(notification),
         }))
-        .filter(({ displayData }) => displayData)
+        .filter(
+          ({ displayData, notification }) =>
+            displayData && notification.status < NotificationStatus.Unseen
+        )
         .slice(-MAX_TOASTS)
     );
   }, [notifications, getKey, getDisplayData]);
 
   if (isMenuOpen) return null;
 
-  const hasMultipleToasts =
-    notificationMap.filter(({ notification }) => notification.status < NotificationStatus.Unseen)
-      .length > 1;
+  const hasMultipleToasts = notificationMap.length > 1;
 
   return (
     <StyledToastArea swipeDirection={isMobile ? 'up' : 'right'} className={className}>
       {hasMultipleToasts && (
-        <StyledOptionButton
+        <StyledToggleButton
           shape={ButtonShape.Pill}
           size={ButtonSize.XSmall}
-          hasStackedNotifs={shouldStackNotifications}
-          action={ButtonAction.Secondary}
-          onClick={() => setshouldStackNotifications(!shouldStackNotifications)}
+          isPressed={shouldStackNotifications}
+          onPressedChange={() => setshouldStackNotifications(!shouldStackNotifications)}
         >
           <ChevronLeftIcon />
-        </StyledOptionButton>
+        </StyledToggleButton>
       )}
 
-      {notificationMap.map(({ notification, key, displayData }) => (
+      {notificationMap.map(({ notification, key, displayData }, idx) => (
         <StyledToast
           key={key}
           isStacked={!isMobile && shouldStackNotifications}
           isOpen={notification.status < NotificationStatus.Unseen}
+          layer={notificationMap.length - 1 - idx}
           notification={notification}
           slotIcon={displayData.icon}
           slotTitle={displayData.title}
@@ -93,7 +95,7 @@ export const NotificationsToastArea = ({ className }: StyleProps) => {
           actionAltText={displayData.actionAltText}
           duration={displayData.toastDuration ?? Infinity}
           sensitivity={displayData.toastSensitivity}
-          setIsOpen={(isOpen: boolean, isClosedFromTimeout: boolean) => {
+          setIsOpen={(isOpen: boolean, isClosedFromTimeout?: boolean) => {
             if (!isOpen)
               if (isClosedFromTimeout)
                 // Toast timer expired without user interaction
@@ -125,20 +127,20 @@ const StyledToastArea = styled(ToastArea)`
 
 const StyledToast = styled(Toast)<{ isStacked?: boolean; layer: number }>`
   // Stacked toast
-  ${({ isStacked }) =>
+  ${({ isStacked, layer }) =>
     isStacked
       ? css`
           position: absolute;
-          top: 0;
           left: 0;
-          right: 0;
+          top: calc(${layer} * 2px);
+          right: calc(${layer} * -2px);
         `
       : css`
           margin-bottom: 0.75rem;
         `}
 `;
 
-const StyledOptionButton = styled(Button)<{ hasStackedNotifs: boolean }>`
+const StyledToggleButton = styled(ToggleButton)<{ isPressed: boolean }>`
   z-index: 2;
   pointer-events: auto;
   display: none;
@@ -150,7 +152,6 @@ const StyledOptionButton = styled(Button)<{ hasStackedNotifs: boolean }>`
 
   ${StyledToastArea}:hover & {
     display: flex;
-    background-color: var(--color-layer-2);
   }
 
   > svg {
@@ -158,8 +159,8 @@ const StyledOptionButton = styled(Button)<{ hasStackedNotifs: boolean }>`
     margin-top: 1px;
     transform: rotate(-90deg);
 
-    ${({ hasStackedNotifs }) =>
-      !hasStackedNotifs &&
+    ${({ isPressed }) =>
+      !isPressed &&
       css`
         transform: rotate(90deg);
       `}
