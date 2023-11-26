ca9501e3cc3fe40d1f04490b83b0a03d034d1511
jaredvu <jaredvu@gmail.com>
2023-11-26 14:55:53 -0800

    Fix orderbook centering

--- src/hooks/orderbook/useCenterOrderbook.ts
+++ src/hooks/orderbook/useCenterOrderbook.ts
@@ -10,10 +10,13 @@ type ElementProps = {
  * Assumed that the two sides are the same height
  */
 export const useCenterOrderbook = ({ marketId, orderbookRef }: ElementProps) => {
+  const orderbookEl = orderbookRef.current;
+  const { clientHeight, scrollHeight } = orderbookEl ?? {};
+  const shouldScroll = !!scrollHeight && !!clientHeight && scrollHeight > clientHeight;
+
   useEffect(() => {
-    if (orderbookRef.current) {
-      const { clientHeight, scrollHeight } = orderbookRef.current;
-      orderbookRef.current.scrollTo({ top: (scrollHeight - clientHeight) / 2 });
+    if (orderbookEl && shouldScroll) {
+      orderbookEl.scrollTo({ top: (scrollHeight - clientHeight) / 2 });
     }
-  }, [orderbookRef.current, marketId]);
+  }, [clientHeight, shouldScroll, marketId]);
 };
