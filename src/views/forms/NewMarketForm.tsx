2327c34bf3a906c3c45d67fd981d06e56e676f6e
jaredvu <jaredvu@gmail.com>
2024-01-17 14:24:57 -0800

    Add mock data

--- src/views/forms/NewMarketForm.tsx
+++ src/views/forms/NewMarketForm.tsx
@@ -1,8 +1,10 @@
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import styled, { AnyStyledComponent } from 'styled-components';
 
-import { layoutMixins } from '@/styles/layoutMixins';
+import { MOCK_DATA } from '@/constants/potentialMarkets';
 import { useDydxClient } from '@/hooks';
+import { layoutMixins } from '@/styles/layoutMixins';
+import { SearchSelectMenu } from '@/components/SearchSelectMenu';
 
 type InputProps = {
   label: string;
@@ -17,6 +19,7 @@ const Input = ({ label, ...props }: InputProps) => (
 
 export const NewMarketForm = () => {
   const { compositeClient } = useDydxClient();
+  const [assetToAdd, setAssetToAdd] = useState<(typeof MOCK_DATA)[number]>();
 
   return (
     <Styled.Form
@@ -25,6 +28,24 @@ export const NewMarketForm = () => {
         // compositeClient?.validatorClient.post.send()
       }}
     >
+      <SearchSelectMenu
+        items={[
+          {
+            group: 'chains',
+            groupLabel: 'Chains',
+            items: MOCK_DATA.map((potentialMarket: (typeof MOCK_DATA)[number]) => ({
+              value: potentialMarket.symbol,
+              label: `${potentialMarket.symbol}-USD`,
+              onSelect: () => {
+                setAssetToAdd(potentialMarket);
+              },
+            })),
+          },
+        ]}
+        label="Chain"
+      >
+        Eth
+      </SearchSelectMenu>
       <Input label="Market Name" />
       <Input label="Market Description" />
       <Input label="Ticker" />
@@ -38,6 +59,7 @@ Styled.Form = styled.form`
   ${layoutMixins.flexColumn}
   gap: 1rem;
 `;
+
 Styled.Label = styled.label`
   background-color: var(--color-layer-5);
   padding: 0.5rem 1rem;
