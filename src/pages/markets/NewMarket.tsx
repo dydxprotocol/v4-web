585f64e660fa7c4b8e0e4c9c1faf09080b3f8ae5
jaredvu <jaredvu@gmail.com>
2024-01-17 16:13:06 -0800

    :lipstick: More UI items

--- src/pages/markets/NewMarket.tsx
+++ src/pages/markets/NewMarket.tsx
@@ -1,20 +1,28 @@
+import { useState } from 'react';
 import styled, { AnyStyledComponent } from 'styled-components';
-
-import { breakpoints } from '@/styles';
+import { CodeIcon } from '@radix-ui/react-icons';
 
 import { useAccountBalance, useBreakpoints, useDocumentTitle, useStringGetter } from '@/hooks';
+import { HelpCircleIcon } from '@/icons';
 
 import { layoutMixins } from '@/styles/layoutMixins';
+import { breakpoints } from '@/styles';
 
+import { Button } from '@/components/Button';
 import { ContentSectionHeader } from '@/components/ContentSectionHeader';
-
 import { NewMarketForm } from '@/views/forms/NewMarketForm';
+
+enum NewMarketDisplay {
+  HOW_TO,
+  DETAILS,
+}
 
 const HOW_TO_ADD_MARKET_TEXT = [
   {
     step: 1,
     title: 'Select market',
-    subtitle: "Search or choose from a list of markets you'd like to add",
+    subtitle:
+      'Search or choose from a list of markets you’d like to add. The list is pre-populated with markets that have been deemed “safe” to add to the Protocol. ',
   },
   {
     step: 2,
@@ -44,6 +52,7 @@ const NewMarket = () => {
   const stringGetter = useStringGetter();
   const { isNotTablet } = useBreakpoints();
   const { nativeTokenBalance } = useAccountBalance();
+  const [displayLeft, setDisplayLeft] = useState(NewMarketDisplay.HOW_TO);
 
   useDocumentTitle('New Market');
 
@@ -56,19 +65,43 @@ const NewMarket = () => {
         />
       </Styled.HeaderSection>
       <Styled.Content>
-        <div>
-          <h2>Steps to create a new market</h2>
-          {/* <div>{nativeTokenBalance.toString()}</div> */}
+        {
+          {
+            [NewMarketDisplay.HOW_TO]: (
+              <div>
+                <Button
+                  slotLeft={<CodeIcon />}
+                  onClick={() => setDisplayLeft(NewMarketDisplay.DETAILS)}
+                >
+                  Show Details
+                </Button>
+                <h2>Steps to create a new market</h2>
+                {/* <div>{nativeTokenBalance.toString()}</div> */}
 
-          {HOW_TO_ADD_MARKET_TEXT.map((item) => (
-            <StepItem
-              key={item.step}
-              step={item.step}
-              title={item.title}
-              subtitle={item.subtitle}
-            />
-          ))}
-        </div>
+                {HOW_TO_ADD_MARKET_TEXT.map((item) => (
+                  <StepItem
+                    key={item.step}
+                    step={item.step}
+                    title={item.title}
+                    subtitle={item.subtitle}
+                  />
+                ))}
+              </div>
+            ),
+            [NewMarketDisplay.DETAILS]: (
+              <div>
+                <Button
+                  onClick={() => setDisplayLeft(NewMarketDisplay.HOW_TO)}
+                  slotLeft={<HelpCircleIcon />}
+                >
+                  Show steps
+                </Button>
+
+                <span>Message details</span>
+              </div>
+            ),
+          }[displayLeft]
+        }
         <Styled.FormContainer>
           <h2>Add Market</h2>
           <NewMarketForm />
@@ -114,7 +147,7 @@ Styled.Content = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   max-width: 1200px;
-  gap: 1rem;
+  gap: 2rem;
 
   h2 {
     font: var(--font-large-medium);
@@ -151,15 +184,16 @@ Styled.Column = styled.div`
 
 Styled.Title = styled.span`
   color: var(--color-text-2);
+  font: var(--font-medium-book);
 `;
 
 Styled.Subtitle = styled.span`
-  color: var(--color-text-1);
+  color: var(--color-text-0);
 `;
 
 Styled.FormContainer = styled.div`
   border-radius: 1rem;
-  background-color: var(--color-layer-4);
+  background-color: var(--color-layer-3);
   padding: 1rem;
 `;
 
--- src/views/forms/NewMarketForm.tsx
+++ src/views/forms/NewMarketForm.tsx
@@ -1,25 +1,46 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useMemo, useState } from 'react';
 import styled, { AnyStyledComponent } from 'styled-components';
+import { Root, Item } from '@radix-ui/react-radio-group';
 
-import { MOCK_DATA } from '@/constants/potentialMarkets';
-import { useDydxClient } from '@/hooks';
-import { layoutMixins } from '@/styles/layoutMixins';
+import { AlertType } from '@/constants/alerts';
+import { ButtonAction, ButtonType } from '@/constants/buttons';
+import { LIQUIDITY_TIERS, MOCK_DATA } from '@/constants/potentialMarkets';
+import { useAccountBalance, useDydxClient } from '@/hooks';
+
+import { AlertMessage } from '@/components/AlertMessage';
+import { Button } from '@/components/Button';
+import { Details } from '@/components/Details';
+import { Output, OutputType } from '@/components/Output';
+import { Tag } from '@/components/Tag';
 import { SearchSelectMenu } from '@/components/SearchSelectMenu';
+import { WithDetailsReceipt } from '@/components/WithDetailsReceipt';
 
-type InputProps = {
-  label: string;
-} & React.InputHTMLAttributes<HTMLInputElement>;
-
-const Input = ({ label, ...props }: InputProps) => (
-  <Styled.Label>
-    <span>{label}</span>
-    <input type="text" {...props} />
-  </Styled.Label>
-);
+import { formMixins } from '@/styles/formMixins';
 
 export const NewMarketForm = () => {
   const { compositeClient } = useDydxClient();
   const [assetToAdd, setAssetToAdd] = useState<(typeof MOCK_DATA)[number]>();
+  const [liquidityTier, setLiquidityTier] = useState<string>();
+  const { nativeTokenBalance } = useAccountBalance();
+
+  const alertMessage = useMemo(() => {
+    if (nativeTokenBalance.lt(10_000)) {
+      return {
+        type: AlertType.Warning,
+        message: 'You need at least 10,000 DYDX to add a market.',
+      };
+    }
+
+    return null;
+  }, [nativeTokenBalance]);
+
+  useEffect(() => {
+    if (assetToAdd) {
+      setLiquidityTier(assetToAdd.liquidityTier);
+    }
+  }, [assetToAdd]);
+
+  console.log(MOCK_DATA);
 
   return (
     <Styled.Form
@@ -28,11 +49,11 @@ export const NewMarketForm = () => {
         // compositeClient?.validatorClient.post.send()
       }}
     >
-      <SearchSelectMenu
+      <Styled.SearchSelectMenu
         items={[
           {
-            group: 'chains',
-            groupLabel: 'Chains',
+            group: 'markets',
+            groupLabel: 'Markets',
             items: MOCK_DATA.map((potentialMarket: (typeof MOCK_DATA)[number]) => ({
               value: potentialMarket.symbol,
               label: `${potentialMarket.symbol}-USD`,
@@ -42,13 +63,93 @@ export const NewMarketForm = () => {
             })),
           },
         ]}
-        label="Chain"
+        label="Market"
       >
-        Eth
-      </SearchSelectMenu>
-      <Input label="Market Name" />
-      <Input label="Market Description" />
-      <Input label="Ticker" />
+        {assetToAdd ? `${assetToAdd?.symbol}-USD` : 'e.g. "BTC-USD"'}
+      </Styled.SearchSelectMenu>
+      {assetToAdd && (
+        <>
+          <div>Populated details</div>
+          <div>
+            <Styled.Root value={liquidityTier} onValueChange={setLiquidityTier}>
+              <Styled.Header>Liquidity tier</Styled.Header>
+
+              {Object.keys(LIQUIDITY_TIERS).map((tier, idx) => {
+                const { maintenanceMarginFraction, impactNotional, label, initialMarginFraction } =
+                  LIQUIDITY_TIERS[tier as unknown as keyof typeof LIQUIDITY_TIERS];
+                return (
+                  <Styled.LiquidityTierRadioButton
+                    key={tier}
+                    value={tier}
+                    selected={tier === liquidityTier}
+                  >
+                    <Styled.Header style={{ marginLeft: '1rem' }}>{label}</Styled.Header>
+                    <Styled.Details
+                      layout="rowColumns"
+                      withSeparators
+                      items={[
+                        {
+                          key: 'imf',
+                          label: 'IMF',
+                          value: (
+                            <Output
+                              fractionDigits={2}
+                              type={OutputType.Number}
+                              value={initialMarginFraction}
+                            />
+                          ),
+                        },
+                        {
+                          key: 'mmf',
+                          label: 'Maintenance margin',
+                          value: (
+                            <Output
+                              fractionDigits={2}
+                              type={OutputType.Number}
+                              value={maintenanceMarginFraction}
+                            />
+                          ),
+                        },
+                        {
+                          key: 'impact-notional',
+                          label: 'Impact Notional',
+                          value: <Output type={OutputType.Number} value={impactNotional} />,
+                        },
+                      ]}
+                    />
+                  </Styled.LiquidityTierRadioButton>
+                );
+              })}
+            </Styled.Root>
+          </div>
+        </>
+      )}
+      {alertMessage && (
+        <AlertMessage type={alertMessage.type}>{alertMessage.message} </AlertMessage>
+      )}
+      <WithDetailsReceipt
+        detailItems={[
+          {
+            key: 'dydx-required',
+            label: (
+              <span>
+                Required balance <Tag>DYDX</Tag>
+              </span>
+            ),
+            value: (
+              <Output
+                type={OutputType.Number}
+                value={10000}
+                slotRight={<Styled.Disclaimer>or more</Styled.Disclaimer>}
+              />
+            ),
+          },
+        ]}
+      >
+        <Button type={ButtonType.Submit} action={ButtonAction.Primary}>
+          Select Market
+        </Button>
+      </WithDetailsReceipt>
     </Styled.Form>
   );
 };
@@ -56,12 +157,43 @@ export const NewMarketForm = () => {
 const Styled: Record<string, AnyStyledComponent> = {};
 
 Styled.Form = styled.form`
-  ${layoutMixins.flexColumn}
-  gap: 1rem;
+  ${formMixins.transfersForm}
 `;
 
-Styled.Label = styled.label`
-  background-color: var(--color-layer-5);
-  padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
+Styled.SearchSelectMenu = styled(SearchSelectMenu)``;
+
+Styled.Disclaimer = styled.div`
+  font: var(--font-small);
+  color: var(--color-text-0);
+  margin-left: 0.5ch;
+`;
+
+Styled.Header = styled.div`
+  color: var(--color-text-2);
+  font: var(--font-base-medium);
+`;
+
+Styled.Root = styled(Root)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+  border-radius: 10px;
+  border: 1px solid var(--color-layer-6);
+  background-color: var(--color-layer-4);
+`;
+
+Styled.LiquidityTierRadioButton = styled(Item)<{ selected?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  border-radius: 0.625rem;
+  border: 1px solid var(--color-layer-6);
+  padding: 1rem 0;
+
+  ${({ selected }) => selected && 'background-color: var(--color-layer-2)'}
+`;
+
+Styled.Details = styled(Details)`
+  margin-top: 0.5rem;
+  padding: 0;
 `;
