7c64301b335643aa65e8ec0c070db1dd6e407ac2
Bill He <bill@dydx.exchange>
2024-01-18 11:23:21 -0800

    bump privy

--- src/hooks/useWalletConnection.ts
+++ src/hooks/useWalletConnection.ts
@@ -118,6 +118,7 @@ export const useWalletConnection = () => {
         if (!walletConnection) {
           throw new Error('Onboarding: No wallet connection found.');
         } else if (walletConnection.type === WalletConnectionType.Privy) {
+
           console.log({ isConnectedWagmi, authenticated, ready });
           if (!isConnectedWagmi && !authenticated && ready) {
             login();
