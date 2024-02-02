0588c89406c2fbe94e7f08df5a33e723ad9b6323
mulan xia <mulanjxia@gmail.com>
2024-02-02 16:23:33 -0500

    bump package + update stringkey

--- src/pages/rewards/LaunchIncentivesPanel.tsx
+++ src/pages/rewards/LaunchIncentivesPanel.tsx
@@ -117,7 +117,7 @@ const LaunchIncentivesContent = () => {
         {stringGetter({ key: STRING_KEYS.LAUNCH_INCENTIVES_DESCRIPTION })}{' '}
       </Styled.Description>
       <Styled.ChaosLabsLogo>
-        {stringGetter({ key: STRING_KEYS.POWERED_BY })} <ChaosLabsIcon />
+        {stringGetter({ key: STRING_KEYS.POWERED_BY_ALL_CAPS })} <ChaosLabsIcon />
       </Styled.ChaosLabsLogo>
       <Styled.ButtonRow>
         <Styled.AboutButton
