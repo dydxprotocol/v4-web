d24a02f7b9cd6bbd87de1966d7233775a4ec05aa
Jared Vu <jaredvu@gmail.com>
2024-02-23 09:44:01 -0800

    Add BrowserRouter support (#258)
    * Add BrowserRouter support
    
    * Handle base route for hashrouter/browserrouter
    
    * Add hashstring fallback
    
    * parseHash -> parseLocationHash

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import './polyfills';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { store } from '@/state/_store';
@@ -12,11 +12,13 @@ import './index.css';
 
 import App from './App';
 
+const Router = import.meta.env.VITE_ROUTER_TYPE === 'hash' ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ErrorBoundary>
     <StrictMode>
       <Provider store={store}>
-        <HashRouter children={<App />} />
+        <Router children={<App />} />
       </Provider>
     </StrictMode>
   </ErrorBoundary>
