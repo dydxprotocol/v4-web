50fb3499f0327fd84f6a7aac1f8ec92f1abbeead
Bill He <bill@dydx.exchange>
2024-03-05 16:53:32 -0800

    ci(tests): Add e2e browserstack testing CI

---
+++ __tests__/e2e/test.e2e.ts
@@ -0,0 +1,15 @@
+import { expect, browser, $ } from '@wdio/globals';
+
+describe('Smoke test', () => {
+  it('should authenticate with vercel and load website', async () => {
+    await browser.url(process.env.E2E_ENVIRONMENT_URL || '');
+    browser.setWindowSize(1920, 1080);
+
+    await $('input[type=password]').setValue(process.env.E2E_ENVIRONMENT_PASSWORD || '');
+    await $('button.submit').click();
+
+    await expect($('main')).toBeExisting();
+    await expect($('header')).toBeExisting();
+    await expect($('footer')).toBeExisting();
+  });
+});
