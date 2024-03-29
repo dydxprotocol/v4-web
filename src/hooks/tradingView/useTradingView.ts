b3d76251d5e86f09b30b707cd8dd15640ab4094a
aleka <aforaleka@gmail.com>
2024-03-29 16:10:07 -0400

    feat(performance): trading view kick off getBars / request candles sooner (#399)
    * remove markets dependency for loading candles
    
    * separate indexer client out
    
    * nits
    
    * address feedback
    
    * add initial price scale
    
    * fix tv chart missing symbol error
    
    * remove stray console.log
    
    * bump abacus and add back settimeout

--- src/hooks/tradingView/useTradingView.ts
+++ src/hooks/tradingView/useTradingView.ts
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import BigNumber from 'bignumber.js';
 import isEmpty from 'lodash/isEmpty';
 import { LanguageCode, ResolutionString, widget } from 'public/tradingview/charting_library';
 import { shallowEqual, useSelector } from 'react-redux';
@@ -41,7 +42,8 @@ export const useTradingView = ({
   const marketIds = useSelector(getMarketIds, shallowEqual);
   const selectedLocale = useSelector(getSelectedLocale);
   const selectedNetwork = useSelector(getSelectedNetwork);
-  const { getCandlesForDatafeed, isConnected: isClientConnected } = useDydxClient();
+
+  const { getCandlesForDatafeed, getMarketTickSize } = useDydxClient();
 
   const [savedTvChartConfig, setTvChartConfig] = useLocalStorage<object | undefined>({
     key: LocalStorageKey.TradingViewChartConfig,
@@ -51,14 +53,28 @@ export const useTradingView = ({
   const savedResolution = getSavedResolution({ savedConfig: savedTvChartConfig });
   const hasMarkets = marketIds.length > 0;
 
+  const [initialPriceScale, setInitialPriceScale] = useState(100);
+
   useEffect(() => {
-    if (hasMarkets && isClientConnected && marketId) {
+    (async () => {
+      if (marketId && !hasMarkets) {
+        const marketTickSize = await getMarketTickSize(marketId);
+        const priceScale = BigNumber(10).exponentiatedBy(
+          BigNumber(marketTickSize).decimalPlaces() ?? 2
+        );
+        setInitialPriceScale(priceScale.toNumber());
+      }
+    })();
+  }, [marketId!!, hasMarkets]);
+
+  useEffect(() => {
+    if (marketId) {
       const widgetOptions = getWidgetOptions();
       const widgetOverrides = getWidgetOverrides({ appTheme, appColorMode });
       const options = {
         ...widgetOptions,
         ...widgetOverrides,
-        datafeed: getDydxDatafeed(store, getCandlesForDatafeed),
+        datafeed: getDydxDatafeed(store, getCandlesForDatafeed, initialPriceScale),
         interval: (savedResolution || DEFAULT_RESOLUTION) as ResolutionString,
         locale: SUPPORTED_LOCALE_BASE_TAGS[selectedLocale] as LanguageCode,
         symbol: marketId,
@@ -97,14 +113,7 @@ export const useTradingView = ({
       tvWidgetRef.current = null;
       setIsChartReady(false);
     };
-  }, [
-    getCandlesForDatafeed,
-    isClientConnected,
-    hasMarkets,
-    selectedLocale,
-    selectedNetwork,
-    !!marketId,
-  ]);
+  }, [selectedLocale, selectedNetwork, initialPriceScale, marketId!!]);
 
   return { savedResolution };
 };
