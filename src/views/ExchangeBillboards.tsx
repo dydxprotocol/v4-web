9b9bbc8160067f2d866d4bc4e9d2a3f1644be340
Davide Segullo <davidesegullo@gmail.com>
2024-04-09 00:51:33 +0200

    feat: :lipstick: add new billboard style

--- src/views/ExchangeBillboards.tsx
+++ src/views/ExchangeBillboards.tsx
@@ -1,83 +1,92 @@
+import { useMemo } from 'react';
+
 import { shallowEqual, useSelector } from 'react-redux';
 import styled, { type AnyStyledComponent } from 'styled-components';
 
 import { STRING_KEYS } from '@/constants/localization';
 
-import { useBreakpoints, useStringGetter } from '@/hooks';
+import { useStringGetter } from '@/hooks';
 
 import { breakpoints } from '@/styles';
 import { layoutMixins } from '@/styles/layoutMixins';
 
 import { Output, OutputType } from '@/components/Output';
+import { Tag } from '@/components/Tag';
 
+import { store } from '@/state/_store';
 import { getPerpetualMarkets } from '@/state/perpetualsSelectors';
 
 type ExchangeBillboardsProps = {
-  isSearching: boolean;
-  searchQuery: string;
   className?: string;
 };
 
-export const ExchangeBillboards: React.FC<ExchangeBillboardsProps> = ({
-  isSearching,
-  searchQuery,
-  className,
-}) => {
+export const ExchangeBillboards: React.FC<ExchangeBillboardsProps> = ({ className }) => {
   const stringGetter = useStringGetter();
-  const { isTablet } = useBreakpoints();
-
-  let volume24HUSDC = 0;
-  let totalTrades24H = 0;
-  let openInterestUSDC = 0;
 
   const perpetualMarkets = useSelector(getPerpetualMarkets, shallowEqual) ?? {};
 
-  Object.values(perpetualMarkets)
-    .filter(Boolean)
-    .forEach(({ oraclePrice, perpetual }) => {
+  const { volume24HUSDC, totalTrades24H, openInterestUSDC } = useMemo(() => {
+    let volume24HUSDC = 0;
+    let totalTrades24H = 0;
+    let openInterestUSDC = 0;
+
+    const markets = Object.values(perpetualMarkets).filter(Boolean);
+
+    for (const { oraclePrice, perpetual } of markets) {
       const { volume24H, trades24H, openInterest = 0 } = perpetual || {};
       volume24HUSDC += volume24H ?? 0;
       totalTrades24H += trades24H ?? 0;
       if (oraclePrice) openInterestUSDC += openInterest * oraclePrice;
-    });
+    }
+
+    return {
+      volume24HUSDC,
+      totalTrades24H,
+      openInterestUSDC,
+    };
+  }, [perpetualMarkets]);
 
   return (
     <Styled.MarketBillboardsWrapper className={className}>
       {[
         {
           key: 'volume',
-          labelKey: isTablet ? STRING_KEYS.VOLUME_24H : STRING_KEYS.TRADING_VOLUME,
+          labelKey: STRING_KEYS.TRADING_VOLUME,
+          tagKey: STRING_KEYS._24H,
           value: volume24HUSDC || undefined,
           fractionDigits: 0,
-          type: isTablet ? OutputType.CompactFiat : OutputType.Fiat,
-          subLabelKey: !isTablet && STRING_KEYS.TRADING_VOLUME_LABEL,
+          type: OutputType.CompactFiat,
         },
         {
           key: 'open-interest',
           labelKey: STRING_KEYS.OPEN_INTEREST,
+          tagKey: STRING_KEYS.CURRENT,
           value: openInterestUSDC || undefined,
           fractionDigits: 0,
-          type: isTablet ? OutputType.CompactFiat : OutputType.Fiat,
-          subLabelKey: !isTablet && STRING_KEYS.OPEN_INTEREST_LABEL,
+          type: OutputType.CompactFiat,
         },
         {
-          key: 'trades',
-          labelKey: isTablet ? STRING_KEYS.TRADES_24H : STRING_KEYS.TRADES,
+          key: 'fee-earned-stakers',
+          labelKey: STRING_KEYS.EARNED,
+          tagKey: STRING_KEYS._24H,
           value: totalTrades24H || undefined,
-          type: isTablet ? OutputType.CompactNumber : OutputType.Number,
-          subLabelKey: !isTablet && STRING_KEYS.TRADES_LABEL,
+          type: OutputType.CompactNumber,
         },
-      ].map(({ key, labelKey, value, fractionDigits, type, subLabelKey }) => (
+      ].map(({ key, labelKey, tagKey, value, fractionDigits, type }) => (
         <Styled.BillboardContainer key={key}>
-          <label>{stringGetter({ key: labelKey })}</label>
-          <Styled.Output
-            useGrouping
-            fractionDigits={fractionDigits}
-            type={type}
-            value={value}
-            withBaseFont
-          />
-          {subLabelKey && <p>{stringGetter({ key: subLabelKey })}</p>}
+          <Styled.BillboardStat>
+            <Styled.BillboardTitle>
+              <label>{stringGetter({ key: labelKey })}</label>
+              <Tag>{stringGetter({ key: tagKey })}</Tag>
+            </Styled.BillboardTitle>
+            <Styled.Output
+              useGrouping
+              fractionDigits={fractionDigits}
+              type={type}
+              value={value}
+              withBaseFont
+            />
+          </Styled.BillboardStat>
         </Styled.BillboardContainer>
       ))}
     </Styled.MarketBillboardsWrapper>
@@ -87,57 +96,38 @@ export const ExchangeBillboards: React.FC<ExchangeBillboardsProps> = ({
 const Styled: Record<string, AnyStyledComponent> = {};
 
 Styled.MarketBillboardsWrapper = styled.div`
-  ${layoutMixins.row}
+  ${layoutMixins.column}
 
-  gap: 1.75rem;
-
-  padding: 1rem;
-
-  @media ${breakpoints.tablet} {
-    gap: 0.625rem;
-
-    padding: 0 1rem;
-  }
+  gap: 1rem;
 `;
 
 Styled.BillboardContainer = styled.div`
   ${layoutMixins.rowColumn}
-  width: 17.5rem;
+  flex: 1;
+`;
+
+Styled.BillboardTitle = styled.div`
+  ${layoutMixins.row}
+
+  gap: 0.375rem;
+`;
+
+Styled.BillboardStat = styled.div`
+  ${layoutMixins.column}
+
+  background-color: var(--color-layer-3);
+  gap: 0.5rem;
+  padding: 1.5rem;
+  border-radius: 0.625rem;
 
   label {
-    margin-bottom: 1rem;
-  }
-
-  p {
-    margin-top: 0.25rem;
-  }
-
-  label,
-  p {
-    font: var(--font-base-book);
     color: var(--color-text-0);
+    font: var(--font-base-medium);
   }
 
-  &:not(:last-child) {
-    border-right: solid var(--border-width) var(--color-border);
-  }
-
-  @media ${breakpoints.tablet} {
-    padding: 0.625rem 0.75rem;
-
-    background-color: var(--color-layer-3);
-    border-radius: 0.625rem;
-
-    &:not(:last-child) {
-      border-right: none;
-    }
-
-    label {
-      margin-bottom: 0.5rem;
-      color: var(--color-text-1);
-
-      font: var(--font-mini-book);
-    }
+  output {
+    color: var(--color-text-1);
+    font: var(--font-large-medium);
   }
 `;
 
