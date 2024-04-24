5123e1135a21aacc61587227a13586745f7a1767
jeremy lee <jeremy@dydx.exchange>
2024-04-24 11:52:19 -0400

    add cctp file i forgot

---
+++ src/constants/cctp.ts
@@ -0,0 +1,8 @@
+import cctpTokens from '../../public/configs/cctp.json';
+
+export const CCTP_TOKEN_NAMES_LOWER_CASE = cctpTokens.map((token) => token.name.toLowerCase());
+
+// TODO: make a general capitalize util fn
+export const CCTP_TOKEN_NAMES_CAPITALIZED = CCTP_TOKEN_NAMES_LOWER_CASE.map(
+  (tokenName) => tokenName[0].toUpperCase() + tokenName.slice(1)
+);
