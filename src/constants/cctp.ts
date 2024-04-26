df7777b617c987ea40c674c6f1ccb2dde750a777
jeremy lee <jeremy@dydx.exchange>
2024-04-26 16:05:47 -0400

    address comment

--- src/constants/cctp.ts
+++ src/constants/cctp.ts
@@ -1,12 +1,11 @@
 import cctpTokens from '../../public/configs/cctp.json';
 
-const CCTP_CHAIN_NAMES_LOWER_CASE = cctpTokens.map((token) => token.name.toLowerCase());
-
-const CCTP_MAINNET_CHAIN_NAMES = CCTP_CHAIN_NAMES_LOWER_CASE.filter(
-  (name) => name === 'Ethereum Goerli'
+const CCTP_MAINNET_CHAINS = cctpTokens.filter((token) => !token.isTestnet);
+const CCTP_MAINNET_CHAINS_NAMES_LOWER_CASE = CCTP_MAINNET_CHAINS.map((token) =>
+  token.name.toLowerCase()
 );
 
 // TODO: make a general capitalize util fn
-export const CCTP_MAINNET_CHAIN_NAMES_CAPITALIZED = CCTP_MAINNET_CHAIN_NAMES.map(
+export const CCTP_MAINNET_CHAIN_NAMES_CAPITALIZED = CCTP_MAINNET_CHAINS_NAMES_LOWER_CASE.map(
   (tokenName) => tokenName[0].toUpperCase() + tokenName.slice(1)
 );
