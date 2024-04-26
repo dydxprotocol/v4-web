ad56a5b91646ec51f0d6d4b9b6cc9bfc82897ab6
Jeremy Lee <37092291+yogurtandjam@users.noreply.github.com>
2024-04-26 17:59:03 -0400

    feat: refresh withdrawals ui to add clarity around fees (OTE-276) (#461)
    * [chore] refresh withdrawals ui to add clarity around fees
    
    * add cctp file i forgot
    
    * add space after comma
    
    * cleanup
    
    * use styled row with gap. rename ExchangeRate style to more descriptive generic RowWithGap
    
    * address comments. add localization, use proper token by address detection
    
    * cleanup
    
    * replace more l8n stuff
    
    * bump l10n package. localize lowest fees highlighted text
    
    * removing unused code
    
    * remove collapsible details and migrate styling
    
    * exclude non mainnet chain
    
    * address comment
    
    * merge conflicts

--- src/constants/tooltips/withdraw.ts
+++ src/constants/tooltips/withdraw.ts
@@ -1,5 +1,7 @@
 import { type TooltipStrings, TOOLTIP_STRING_KEYS } from '@/constants/localization';
 
+import { CCTP_MAINNET_CHAIN_NAMES_CAPITALIZED } from '../cctp';
+
 export const withdrawTooltips: TooltipStrings = {
   'fast-withdraw-fee': ({ stringGetter }) => ({
     title: stringGetter({ key: TOOLTIP_STRING_KEYS.FAST_WITHDRAW_FEE_TITLE }),
@@ -12,5 +14,22 @@ export const withdrawTooltips: TooltipStrings = {
   'withdraw-types': ({ stringGetter }) => ({
     title: stringGetter({ key: TOOLTIP_STRING_KEYS.WITHDRAW_TYPES_TITLE }),
     body: stringGetter({ key: TOOLTIP_STRING_KEYS.WITHDRAW_TYPES_BODY }),
+  }),
+  'gas-fees': ({ stringGetter }) => ({
+    title: stringGetter({ key: TOOLTIP_STRING_KEYS.GAS_FEES_TITLE }),
+    body: stringGetter({ key: TOOLTIP_STRING_KEYS.GAS_FEES_BODY }),
+  }),
+  'bridge-fees': ({ stringGetter }) => ({
+    title: stringGetter({ key: TOOLTIP_STRING_KEYS.BRIDGE_FEES_TITLE }),
+    body: stringGetter({ key: TOOLTIP_STRING_KEYS.BRIDGE_FEES_BODY }),
+  }),
+  'lowest-fees': ({ stringGetter }) => ({
+    title: stringGetter({ key: TOOLTIP_STRING_KEYS.LOWEST_FEE_WITHDRAWALS_TITLE }),
+    body: stringGetter({
+      key: TOOLTIP_STRING_KEYS.LOWEST_FEE_WITHDRAWALS_BODY,
+      params: {
+        LOWEST_FEE_TOKEN_NAMES: CCTP_MAINNET_CHAIN_NAMES_CAPITALIZED.join(', '),
+      },
+    }),
   }),
 } as const;
