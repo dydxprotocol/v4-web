fac3e231276539b26530769d4241d305e33118fe
aforaleka <aforaleka@gmail.com>
2024-04-30 11:07:53 -0400

    order status notif preferences + refactor

--- src/hooks/useNotificationTypes.tsx
+++ src/hooks/useNotificationTypes.tsx
@@ -92,12 +92,14 @@ export const notificationTypes: NotificationTypeConfig[] = [
 
           switch (abacusNotificationType) {
             case 'order': {
-              const order = ordersById[id]?.[0];
-              const clientId: number | undefined = order?.clientId ?? undefined;
-              const localOrderExists =
-                clientId && localOrdersData.some((order) => order.clientId === clientId);
+              if (notificationPreferences[NotificationType.OrderStatus]) {
+                const order = ordersById[id]?.[0];
+                const clientId: number | undefined = order?.clientId ?? undefined;
+                const localOrderExists =
+                  clientId && localOrdersData.some((order) => order.clientId === clientId);
 
-              if (localOrderExists) return; // already handled by OrderStatusNotification
+                if (localOrderExists) return; // already handled by OrderStatusNotification
+              }
 
               trigger(
                 abacusNotif.id,
