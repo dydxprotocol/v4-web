2900d8c2b39812f1b247a6d0f007b30071ecad52
jeremy lee <jeremy@dydx.exchange>
2024-05-02 09:48:10 -0400

    change script to use relative pathing

--- scripts/install-local-abacus.js
+++ scripts/install-local-abacus.js
@@ -5,23 +5,25 @@ const clean = process.argv.includes('--clean');
 if (clean) {
   infoMessage('Running deep clean.');
   nonFatalExec('pnpm remove @dydxprotocol/v4-abacus'); // remove abacus from node_modules
-  nonFatalExec('cd ~/v4-abacus && ./gradlew clean'); // cleanup gradle build outputs
-  nonFatalExec('cd ~/v4-abacus && ./gradlew --stop'); // stop any running gradle daemons
+  nonFatalExec('cd ../v4-abacus && ./gradlew clean'); // cleanup gradle build outputs
+  nonFatalExec('cd ../v4-abacus && ./gradlew --stop'); // stop any running gradle daemons
   nonFatalExec('rm -rf ~/.gradle/caches'); // nuke the gradle cache
 }
 
 infoMessage('Cleaning up any previously built abacus packages...');
-nonFatalExec('rm ~/v4-abacus/build/packages/*.tgz');
+nonFatalExec('rm ../v4-abacus/build/packages/*.tgz');
 
 infoMessage('Building abacus...');
-fatalExec('cd ~/v4-abacus && ./gradlew packJsPackage');
+fatalExec('cd ../v4-abacus && ./gradlew packJsPackage');
 
 infoMessage('Installing local abacus package...');
-fatalExec("find ~/v4-abacus/build/packages -name '*.tgz' | head -n 1 | xargs pnpm install");
+fatalExec("find ../v4-abacus/build/packages -name '*.tgz' | head -n 1 | xargs pnpm install");
 infoMessage('Successfully installed local abacus package.');
 
 infoMessage('Generating local-abacus-hash...');
-fatalExec("find ~/v4-abacus/build/packages -name '*.tgz' | head -n 1 | shasum > local-abacus-hash");
+fatalExec(
+  "find ../v4-abacus/build/packages -name '*.tgz' | head -n 1 | shasum > local-abacus-hash"
+);
 
 infoMessage('Vite dev server should have restarted automatically.');
 
