cefe4db1e52f5baff7bf3d9682f13b79ca2cd36e
jeremy lee <jeremy@dydx.exchange>
2024-05-07 16:42:17 -0400

    add referrer to every tracking action

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -5,6 +5,8 @@ import type {
   AnalyticsUserPropertyValue,
 } from '@/constants/analytics';
 
+import { testFlags } from './testFlags';
+
 const DEBUG_ANALYTICS = false;
 
 export const identify = <T extends AnalyticsUserProperty>(
@@ -25,11 +27,12 @@ export const track = <T extends AnalyticsEvent>(
   eventType: T,
   eventData?: AnalyticsEventData<T>
 ) => {
+  const eventDataWithReferrer = { ...(eventData || {}), refferer: testFlags.referrer };
   if (DEBUG_ANALYTICS) {
-    console.log(`[Analytics] ${eventType}`, eventData);
+    console.log(`[Analytics] ${eventType}`, eventDataWithReferrer);
   }
   const customEvent = new CustomEvent('dydx:track', {
-    detail: { eventType, eventData },
+    detail: { eventType, eventDataWithReferrer },
   });
 
   globalThis.dispatchEvent(customEvent);
