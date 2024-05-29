0a885c1c9460708ed533f620c3ceaa12fbba8e8a
aforaleka <aforaleka@gmail.com>
2024-05-29 12:23:58 -0400

    abacus bump

--- src/pages/portfolio/Fees.tsx
+++ src/pages/portfolio/Fees.tsx
@@ -13,7 +13,6 @@ import { useStringGetter } from '@/hooks/useStringGetter';
 
 import breakpoints from '@/styles/breakpoints';
 import { layoutMixins } from '@/styles/layoutMixins';
-import { tableMixins } from '@/styles/tableMixins';
 
 import { AttachedExpandingSection } from '@/components/ContentSection';
 import { ContentSectionHeader } from '@/components/ContentSectionHeader';
@@ -189,8 +188,6 @@ const $ContentWrapper = styled.div`
 `;
 
 const $AdditionalConditions = styled.div`
-  ${tableMixins.stackedWithSecondaryStyling}
-
   color: var(--color-text-0);
   font: var(--font-small-book);
 
