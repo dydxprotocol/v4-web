4596f24a8740d9d5dd4513663607761bee694d06
moo-onthelawn <70078372+moo-onthelawn@users.noreply.github.com>
2024-06-03 11:07:50 -0400

    chore: update trading rewards table styling, pr followups (#602)

--- src/pages/token/rewards/RewardsPage.tsx
+++ src/pages/token/rewards/RewardsPage.tsx
@@ -30,6 +30,8 @@ const RewardsPage = () => {
   const { isTablet, isNotTablet } = useBreakpoints();
   const navigate = useNavigate();
 
+  const showChartPanel = testFlags.tradingRewardsRehaul;
+
   return (
     <div>
       {isTablet && (
@@ -39,14 +41,17 @@ const RewardsPage = () => {
         />
       )}
       <DetachedSection>
-        <$GridLayout showMigratePanel={import.meta.env.VITE_V3_TOKEN_ADDRESS && isNotTablet}>
+        <$GridLayout
+          showMigratePanel={import.meta.env.VITE_V3_TOKEN_ADDRESS && isNotTablet}
+          showChartPanel={showChartPanel}
+        >
           {import.meta.env.VITE_V3_TOKEN_ADDRESS && isNotTablet && <$MigratePanel />}
 
           {isTablet ? (
             <$LaunchIncentivesPanel />
           ) : (
             <>
-              {testFlags.tradingRewardsRehaul && <$TradingRewardsChartPanel />}
+              {showChartPanel && <$TradingRewardsChartPanel />}
               <$LaunchIncentivesPanel />
               {testFlags.enableStaking ? <$StakingPanel /> : <$DYDXBalancePanel />}
             </>
@@ -54,7 +59,7 @@ const RewardsPage = () => {
 
           <$TradingRewardsColumn>
             <TradingRewardsSummaryPanel />
-            {isTablet && testFlags.tradingRewardsRehaul && <TradingRewardsChartPanel />}
+            {isTablet && showChartPanel && <TradingRewardsChartPanel />}
             {isTablet && <RewardsHelpPanel />}
             <RewardHistoryPanel />
           </$TradingRewardsColumn>
@@ -72,7 +77,7 @@ const RewardsPage = () => {
 
 export default RewardsPage;
 
-const $GridLayout = styled.div<{ showMigratePanel?: boolean }>`
+const $GridLayout = styled.div<{ showMigratePanel?: boolean; showChartPanel?: boolean }>`
   --gap: 1.5rem;
   display: grid;
   grid-template-columns: 2fr 1fr;
@@ -83,9 +88,8 @@ const $GridLayout = styled.div<{ showMigratePanel?: boolean }>`
     gap: var(--gap);
   }
 
-  // xccx figure out migrate
-  ${({ showMigratePanel }) =>
-    showMigratePanel
+  ${({ showMigratePanel, showChartPanel }) =>
+    showMigratePanel && showChartPanel
       ? css`
           grid-template-areas:
             'migrate migrate'
@@ -94,12 +98,25 @@ const $GridLayout = styled.div<{ showMigratePanel?: boolean }>`
             'balance balance'
             'rewards other';
         `
-      : css`
-          grid-template-areas:
-            'chart chart'
-            'incentives balance'
-            'rewards other';
-        `}
+      : showMigratePanel
+        ? css`
+            grid-template-areas:
+              'migrate migrate'
+              'incentives balance'
+              'rewards other';
+          `
+        : showChartPanel
+          ? css`
+              grid-template-areas:
+                'chart chart'
+                'incentives balance'
+                'rewards other';
+            `
+          : css`
+              grid-template-areas:
+                'incentives balance'
+                'rewards other';
+            `}
 
   @media ${breakpoints.notTablet} {
     padding: 1rem;
@@ -114,6 +131,10 @@ const $GridLayout = styled.div<{ showMigratePanel?: boolean }>`
     grid-template-areas:
       'incentives'
       'rewards';
+
+    > :last-child {
+      margin-bottom: var(--gap);
+    }
   }
 `;
 
