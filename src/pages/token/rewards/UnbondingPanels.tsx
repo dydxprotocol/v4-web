01c9df42a5b9fa41797e82c628541b714b6da0a4
mulan xia <mulanjxia@gmail.com>
2024-06-07 16:34:22 -0400

    fix dup keys

--- src/pages/token/rewards/UnbondingPanels.tsx
+++ src/pages/token/rewards/UnbondingPanels.tsx
@@ -42,7 +42,7 @@ export const UnbondingPanels = () => {
 
         return (
           <Panel
-            key={delegation.validator}
+            key={`${delegation.validator}-${timeDifference}`}
             slotHeaderContent={
               <>
                 <$Title>
