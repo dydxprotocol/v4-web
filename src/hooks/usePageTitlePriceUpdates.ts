06a7615ab181956df1a0bf7943c19cd829cf2cc8
aforaleka <aforaleka@gmail.com>
2024-06-07 18:26:41 -0400

    locale string with both min/max fraction digits

--- src/hooks/usePageTitlePriceUpdates.ts
+++ src/hooks/usePageTitlePriceUpdates.ts
@@ -6,6 +6,7 @@ import { SMALL_USD_DECIMALS } from '@/constants/numbers';
 import { DEFAULT_DOCUMENT_TITLE } from '@/constants/routes';
 
 import { useAppSelector } from '@/state/appTypes';
+import { getSelectedLocale } from '@/state/localizationSelectors';
 import {
   getCurrentMarketConfig,
   getCurrentMarketId,
@@ -13,11 +14,10 @@ import {
   getCurrentMarketOraclePrice,
 } from '@/state/perpetualsSelectors';
 
-import { MustBigNumber } from '@/lib/numbers';
-
 import { useBreakpoints } from './useBreakpoints';
 
 export const usePageTitlePriceUpdates = () => {
+  const selectedLocale = useAppSelector(getSelectedLocale);
   const { isNotTablet } = useBreakpoints();
   const id = useAppSelector(getCurrentMarketId);
   const oraclePrice = useAppSelector(getCurrentMarketOraclePrice);
@@ -29,7 +29,10 @@ export const usePageTitlePriceUpdates = () => {
 
   useEffect(() => {
     if (id && price && isNotTablet) {
-      const priceString = MustBigNumber(price).toFixed(tickSizeDecimals ?? SMALL_USD_DECIMALS);
+      const priceString = price.toLocaleString(selectedLocale, {
+        minimumFractionDigits: tickSizeDecimals ?? SMALL_USD_DECIMALS,
+        maximumFractionDigits: tickSizeDecimals ?? SMALL_USD_DECIMALS,
+      });
       document.title = `$${priceString} ${id} · ${DEFAULT_DOCUMENT_TITLE}`;
     } else {
       document.title = DEFAULT_DOCUMENT_TITLE;
