6371bc38589ce34fe719e2a0b595758ce82922c5
aforaleka <aforaleka@gmail.com>
2024-06-07 16:23:48 -0400

    add min fraction digits to price for page title

--- src/hooks/usePageTitlePriceUpdates.ts
+++ src/hooks/usePageTitlePriceUpdates.ts
@@ -1,10 +1,14 @@
 import { useEffect } from 'react';
 
+import { shallowEqual } from 'react-redux';
+
+import { SMALL_USD_DECIMALS } from '@/constants/numbers';
 import { DEFAULT_DOCUMENT_TITLE } from '@/constants/routes';
 
 import { useAppSelector } from '@/state/appTypes';
 import { getSelectedLocale } from '@/state/localizationSelectors';
 import {
+  getCurrentMarketConfig,
   getCurrentMarketId,
   getCurrentMarketMidMarketPrice,
   getCurrentMarketOraclePrice,
@@ -17,13 +21,17 @@ export const usePageTitlePriceUpdates = () => {
   const { isNotTablet } = useBreakpoints();
   const id = useAppSelector(getCurrentMarketId);
   const oraclePrice = useAppSelector(getCurrentMarketOraclePrice);
+  const { tickSizeDecimals } = useAppSelector(getCurrentMarketConfig, shallowEqual) ?? {};
+
   const orderbookMidMarketPrice = useAppSelector(getCurrentMarketMidMarketPrice);
 
   const price = orderbookMidMarketPrice ?? oraclePrice;
 
   useEffect(() => {
     if (id && price && isNotTablet) {
-      const priceString = price.toLocaleString(selectedLocale);
+      const priceString = price.toLocaleString(selectedLocale, {
+        minimumFractionDigits: tickSizeDecimals ?? SMALL_USD_DECIMALS,
+      });
       document.title = `$${priceString} ${id} · ${DEFAULT_DOCUMENT_TITLE}`;
     } else {
       document.title = DEFAULT_DOCUMENT_TITLE;
