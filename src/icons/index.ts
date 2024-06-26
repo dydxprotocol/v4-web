94ddc5f0c876ca4da7a55934ace73dac033c7fc2
jeremy lee <jeremy@dydx.exchange>
2024-06-26 18:44:43 -0400

    comment for clarity

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -103,6 +103,8 @@ export { default as WebsiteIcon } from './website.svg';
 export { default as WhitepaperIcon } from './whitepaper.svg';
 
 // Logos
+// Commented out because of https://github.com/dydxprotocol/v4-web/pull/737
+// Left as comment for context in case people wonder why they're not imported here
 // export { default as ChaosLabsIcon } from './chaos-labs';
 // export { default as LogoShortIcon } from './logo-short';
 export { default as EtherscanIcon } from './logos/etherscan.svg';
