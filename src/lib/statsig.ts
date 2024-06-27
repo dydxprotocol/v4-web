c5b0eecb6923a5e7263584406e3feb4fd1ac807c
jeremy lee <jeremy@dydx.exchange>
2024-06-26 22:09:51 -0400

    test friendly

--- src/lib/statsig.ts
+++ src/lib/statsig.ts
@@ -4,11 +4,26 @@ const fetchIp = async () => {
   const resp = await fetch('https://api.ipify.org?format=json');
   return resp.json();
 };
-const ip = await fetchIp();
-export const statsigClient = new StatsigClient(import.meta.env.VITE_STATSIG_CLIENT_KEY, {
-  ip,
-});
-await statsigClient.initializeAsync();
+
+const initStatsig = async () => {
+  const statsigClient = new StatsigClient(
+    import.meta.env.VITE_STATSIG_CLIENT_KEY,
+    {},
+    {
+      disableLogging: import.meta.env.VITE_DISABLE_STATSIG,
+      disableStorage: import.meta.env.VITE_DISABLE_STATSIG,
+    }
+  );
+  if (import.meta.env.VITE_DISABLE_STATSIG) return statsigClient;
+  const ip = await fetchIp();
+  statsigClient.updateUserSync({
+    ip,
+  });
+  await statsigClient.initializeSync();
+  return statsigClient;
+};
+
+export const statsigClient = await initStatsig();
 
 export enum StatSigFlags {
   // When adding a flag here, make sure to add an analytics tracker in useAnalytics.ts
