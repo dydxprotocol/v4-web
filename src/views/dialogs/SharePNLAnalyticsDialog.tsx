475e4fbe4e8021f97db2a05f67fb4b4dbe9af0c5
mulan xia <mulanjxia@gmail.com>
2024-07-03 15:54:34 -0400

    add analytics for share

--- src/views/dialogs/SharePNLAnalyticsDialog.tsx
+++ src/views/dialogs/SharePNLAnalyticsDialog.tsx
@@ -3,6 +3,7 @@ import { useMemo } from 'react';
 import { useToBlob } from '@hugocxl/react-to-image';
 import styled from 'styled-components';
 
+import { AnalyticsEvents } from '@/constants/analytics';
 import { ButtonAction } from '@/constants/buttons';
 import { DialogProps, SharePNLAnalyticsDialogProps } from '@/constants/dialogs';
 import { STRING_KEYS } from '@/constants/localization';
@@ -24,6 +25,7 @@ import { Tag, TagSign } from '@/components/Tag';
 import { useAppDispatch } from '@/state/appTypes';
 import { closeDialog } from '@/state/dialogs';
 
+import { track } from '@/lib/analytics';
 import { MustBigNumber } from '@/lib/numbers';
 import { triggerTwitterIntent } from '@/lib/twitter';
 
@@ -166,6 +168,7 @@ export const SharePNLAnalyticsDialog = ({
           action={ButtonAction.Secondary}
           slotLeft={<Icon iconName={IconName.Copy} />}
           onClick={() => {
+            track(AnalyticsEvents.SharePnlCopied({ asset: assetId }));
             convert();
           }}
           state={{
@@ -178,6 +181,7 @@ export const SharePNLAnalyticsDialog = ({
           action={ButtonAction.Primary}
           slotLeft={<Icon iconName={IconName.SocialX} />}
           onClick={() => {
+            track(AnalyticsEvents.SharePnlShared({ asset: assetId }));
             convertShare();
           }}
           state={{
