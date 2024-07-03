befc7117ffeda03ff1c1951a720e8fa609c63e3c
jeremy lee <jeremy@dydx.exchange>
2024-07-02 22:31:49 -0400

    await the promise instead of returning unintialized client

--- src/hooks/useInitializePage.ts
+++ src/hooks/useInitializePage.ts
@@ -8,9 +8,9 @@ import { useAppDispatch } from '@/state/appTypes';
 
 import abacusStateManager from '@/lib/abacus';
 import { validateAgainstAvailableEnvironments } from '@/lib/network';
+import { statsigConfigPromise } from '@/lib/statsig';
 
 import { useLocalStorage } from './useLocalStorage';
-import { useAllStatsigGateValues } from './useStatsig';
 
 export const useInitializePage = () => {
   const dispatch = useAppDispatch();
@@ -21,13 +21,15 @@ export const useInitializePage = () => {
     defaultValue: DEFAULT_APP_ENVIRONMENT,
     validateFn: validateAgainstAvailableEnvironments,
   });
-  const statsigConfig = useAllStatsigGateValues();
 
   useEffect(() => {
     dispatch(initializeLocalization());
-    console.log(statsigConfig);
-    abacusStateManager.setStatsigConfigs(statsigConfig);
-    // Set store so (Abacus & v4-Client) classes can getState and dispatch
-    abacusStateManager.start({ network: localStorageNetwork });
-  }, [dispatch, localStorageNetwork, statsigConfig]);
+    const start = async () => {
+      const statsigConfig = await statsigConfigPromise();
+      abacusStateManager.setStatsigConfigs(statsigConfig);
+      // Set store so (Abacus & v4-Client) classes can getState and dispatch
+      abacusStateManager.start({ network: localStorageNetwork });
+    };
+    start();
+  }, []);
 };
--- src/lib/statsig.ts
+++ src/lib/statsig.ts
@@ -1,20 +1,37 @@
+import { StatSigFlags, StatsigConfigType } from '@/types/statsig';
 import { StatsigClient } from '@statsig/js-client';
 
 let statsigClient: StatsigClient;
+let initPromise: Promise<StatsigClient> | null = null;
 
 export const initStatsig = async () => {
+  if (initPromise) {
+    return initPromise;
+  }
   if (statsigClient) return statsigClient;
-  statsigClient = new StatsigClient(
-    import.meta.env.VITE_STATSIG_CLIENT_KEY ?? '',
-    { userID: 'test-id' },
-    {
-      disableLogging: import.meta.env.VITE_DISABLE_STATSIG,
-      disableStorage: import.meta.env.VITE_DISABLE_STATSIG,
-    }
-  );
-  await statsigClient.initializeAsync();
-  return statsigClient;
+
+  initPromise = (async () => {
+    statsigClient = new StatsigClient(
+      import.meta.env.VITE_STATSIG_CLIENT_KEY ?? '',
+      { userID: 'test-id' },
+      {
+        disableLogging: import.meta.env.VITE_DISABLE_STATSIG,
+        disableStorage: import.meta.env.VITE_DISABLE_STATSIG,
+      }
+    );
+    await statsigClient.initializeAsync();
+    return statsigClient;
+  })();
+  return initPromise;
 };
 
 // TODO: figure out how to init statsig async so we get configs on the first page load without breaking app state
 // https://linear.app/dydx/project/feature-experimentation-6853beb333d7/overview
+
+export const statsigConfigPromise = async () => {
+  const client = await initStatsig();
+  const allGateValues = Object.values(StatSigFlags).reduce((acc, gate) => {
+    return { ...acc, [gate]: client.checkGate(gate) };
+  }, {} as StatsigConfigType);
+  return allGateValues;
+};
