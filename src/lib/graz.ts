eeae30332f28cbafd4722045bece4b45f12703d4
devmosis <ryzond6728@gmail.com>
2024-07-08 17:19:18 +0900

    chore: bump graz

--- src/lib/graz.ts
+++ src/lib/graz.ts
@@ -1,10 +1,11 @@
 import { BECH32_PREFIX, NOBLE_BECH32_PREFIX } from '@dydxprotocol/v4-client-js';
+import { type ConfigureGrazArgs } from 'graz';
 
 import { isMainnet } from '@/constants/networks';
 
 import { getNobleChainId } from './squid';
 
-export const config = {
+export const config: ConfigureGrazArgs = {
   chains: [
     {
       chainId: isMainnet ? 'dydx-mainnet-1' : 'dydx-testnet-4',
@@ -13,5 +14,5 @@ export const config = {
       },
     },
     { chainId: getNobleChainId(), bech32Config: { bech32PrefixAccAddr: NOBLE_BECH32_PREFIX } },
-  ],
+  ] as ConfigureGrazArgs['chains'],
 };
