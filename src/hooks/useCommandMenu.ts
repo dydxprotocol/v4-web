951e1bd6f2e5636599d2210e776782cf62ddf51a
aleka <aforaleka@gmail.com>
2024-07-10 14:33:09 -0400

    chore: improve cmd-k placement + cleanup (#791)

--- src/hooks/useCommandMenu.ts
+++ src/hooks/useCommandMenu.ts
@@ -1,17 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const useCommandMenu = () => {
   const [isCommandMenuOpen, setIsCommandMenuOpen] = useState(false);
 
+  const handleKeyDown = useCallback((event: KeyboardEvent) => {
+    if (event.key === 'k' && (event.ctrlKey || event.metaKey)) {
+      setIsCommandMenuOpen((isOpen) => !isOpen);
+    }
+  }, []);
+
   useEffect(() => {
-    document.addEventListener('keydown', (event: KeyboardEvent) => {
-      if (event.key === 'k' && (event.ctrlKey || event.metaKey))
-        setIsCommandMenuOpen(!isCommandMenuOpen);
-    });
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
+  const closeCommandMenu = useCallback(() => {
+    setIsCommandMenuOpen(false);
   }, []);
 
   return {
-    closeCommandMenu: () => setIsCommandMenuOpen(false),
+    closeCommandMenu,
     isCommandMenuOpen,
     setIsCommandMenuOpen,
   };
