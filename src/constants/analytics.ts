d01b0e0c9d3f7f0939839474dda5954f16c9d4fa
jeremy lee <jeremy@dydx.exchange>
2024-07-17 17:24:29 -0400

    remove referrer event item. analytics already tracks utm_source

--- src/constants/analytics.ts
+++ src/constants/analytics.ts
@@ -11,13 +11,10 @@ import { TransferNotificationTypes } from './notifications';
 import type { TradeTypes } from './trade';
 import type { DydxAddress, EvmAddress, WalletConnectionType, WalletType } from './wallets';
 
-type AnalyticsEventDataWithReferrer<T extends AnalyticsEventTypes> = AnalyticsEventPayloads[T] & {
-  referrer: string;
-};
 export type AnalyticsEventTrackMeta<T extends AnalyticsEventTypes> = {
   detail: {
     eventType: T;
-    eventData: AnalyticsEventDataWithReferrer<T>;
+    eventData: AnalyticsEventPayloads[T];
   };
 };
 export type AnalyticsEventIdentifyMeta<T extends AnalyticsUserPropertyTypes> = {
@@ -78,6 +75,7 @@ export const AnalyticsUserPropertyLoggableTypes = {
   Breakpoint: 'breakpoint',
   Version: 'version',
   StatsigFlags: 'statsigFlags',
+  CustomDomainReferrer: 'customDomainReferrer',
   Network: 'network',
   WalletType: 'walletType',
   WalletConnectionType: 'walletConnectionType',
