0a219c5d839f23bb7c99525f78051cce0c5cad15
Tyler <tyler@dydx.exchange>
2024-07-22 12:32:59 -0400

    class helper

---
+++ src/lib/classed.tsx
@@ -0,0 +1,26 @@
+import React, { ComponentProps } from 'react';
+
+import { twMerge } from 'tailwind-merge';
+
+interface ExoticComponentWithDisplayName<P extends object = {}> extends React.ExoticComponent<P> {
+  defaultProps?: Partial<P> | undefined;
+  displayName?: string | undefined;
+}
+
+type AnyComponent<P extends object = any> =
+  | ExoticComponentWithDisplayName<P>
+  | React.ComponentType<P>;
+
+export const twClassed = <TargetProps extends { className?: string; children?: React.ReactNode }>(
+  Component: AnyComponent<TargetProps>,
+  boundClassName?: string
+) => {
+  return (props: ComponentProps<typeof Component>) => {
+    const { className, children } = props;
+    return (
+      <Component {...props} className={twMerge(boundClassName, className)}>
+        {children}
+      </Component>
+    );
+  };
+};
