b2507d95ac423a9e64d9b7d8fe005388f65821d8
mulan xia <mulanjxia@gmail.com>
2024-07-24 14:27:25 -0400

    lint

--- src/constants/tooltips/tradeChart.ts
+++ src/constants/tooltips/tradeChart.ts
@@ -1,7 +1,7 @@
 import { TOOLTIP_STRING_KEYS, type TooltipStrings } from '@/constants/localization';
 
 export const tradeChartTooltips: TooltipStrings = {
-  'ohlc': ({ stringGetter }) => ({
+  ohlc: ({ stringGetter }) => ({
     title: stringGetter({ key: TOOLTIP_STRING_KEYS.OHLC_TITLE }),
     body: stringGetter({ key: TOOLTIP_STRING_KEYS.OHLC_BODY }),
   }),
