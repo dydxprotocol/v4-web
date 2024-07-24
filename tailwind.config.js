9f6873acc6faaf2a96e811d5088b8ae8c70c5b35
Tyler <tyler@dydx.exchange>
2024-07-23 20:20:11 -0400

    oops

--- tailwind.config.js
+++ tailwind.config.js
@@ -189,7 +189,7 @@ export default {
         },
         '.stack ': {
           display: 'grid',
-          gridTemplateAreas: 'stack',
+          gridTemplateAreas: `'stack'`,
           '> *, &:before, &:after': {
             gridArea: 'stack',
           },
