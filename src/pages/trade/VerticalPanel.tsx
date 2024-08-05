fa4197652b0bdb0ed39835eacbb0d7be302cb352
moo-onthelawn <70078372+moo-onthelawn@users.noreply.github.com>
2024-08-05 15:26:06 -0400

    chore(orderbook): update orderbook to not be scrollable (#846)

--- src/pages/trade/VerticalPanel.tsx
+++ src/pages/trade/VerticalPanel.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import { TradeLayouts } from '@/constants/layout';
 import { STRING_KEYS } from '@/constants/localization';
+import { ORDERBOOK_HEADER_HEIGHT, ORDERBOOK_ROW_HEIGHT } from '@/constants/orderbook';
 
 import { useStringGetter } from '@/hooks/useStringGetter';
 
@@ -23,6 +24,42 @@ const HISTOGRAM_SIDES_BY_LAYOUT = {
 export const VerticalPanel = ({ tradeLayout }: { tradeLayout: TradeLayouts }) => {
   const stringGetter = useStringGetter();
   const [value, setValue] = useState(Tab.Orderbook);
+  const [rowsPerSide, setRowsPerSide] = useState<number | undefined>(undefined);
+
+  const canvasOrderbookRef = useRef<HTMLDivElement>(null);
+  const canvasOrderbook = canvasOrderbookRef.current;
+
+  const calculateNumRows = useCallback((orderbookHeight: number) => {
+    const maxNumRowsToRender = Math.floor(
+      (orderbookHeight - ORDERBOOK_HEADER_HEIGHT - ORDERBOOK_ROW_HEIGHT) /
+        (2 * ORDERBOOK_ROW_HEIGHT)
+    );
+    setRowsPerSide(maxNumRowsToRender);
+  }, []);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.contentBoxSize[0]) {
+          calculateNumRows(entry.contentBoxSize[0].blockSize);
+        } else {
+          calculateNumRows(entry.contentRect.height);
+        }
+      });
+    });
+
+    if (canvasOrderbook) {
+      resizeObserver.observe(canvasOrderbook);
+    }
+
+    return () => {
+      if (canvasOrderbook) {
+        resizeObserver.unobserve(canvasOrderbook);
+      } else {
+        resizeObserver.disconnect();
+      }
+    };
+  }, [calculateNumRows, canvasOrderbook]);
 
   return (
     <Tabs
@@ -34,10 +71,16 @@ export const VerticalPanel = ({ tradeLayout }: { tradeLayout: TradeLayouts }) =>
       items={[
         {
           asChild: true,
-          content: <CanvasOrderbook histogramSide={HISTOGRAM_SIDES_BY_LAYOUT[tradeLayout]} />,
+          content: (
+            <CanvasOrderbook
+              histogramSide={HISTOGRAM_SIDES_BY_LAYOUT[tradeLayout]}
+              rowsPerSide={rowsPerSide}
+            />
+          ),
           label: stringGetter({ key: STRING_KEYS.ORDERBOOK_SHORT }),
           value: Tab.Orderbook,
           forceMount: true,
+          ref: canvasOrderbookRef,
         },
         {
           content: <LiveTrades histogramSide={HISTOGRAM_SIDES_BY_LAYOUT[tradeLayout]} />,
