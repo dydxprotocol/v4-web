d99af3a99125658c11ef025f2b0dfbcac3e36b8d
Tyler <tyler@dydx.exchange>
2024-08-28 12:56:42 -0400

    test

--- src/lib/__test__/vanillaHooks.spec.ts
+++ src/lib/__test__/vanillaHooks.spec.ts
@@ -47,4 +47,66 @@ describe('test hooks', () => {
     expect(handleInternal).toHaveBeenCalledOnce();
     expect(fn(1)).toEqual(2);
   });
+
+  it('syncs store', async () => {
+    let subs: Array<() => void> = [];
+    const addSubOne = vi.fn().mockImplementation((onChange: () => void) => {
+      subs.push(onChange);
+      return () => {
+        subs = subs.filter((s) => s !== onChange);
+      };
+    });
+    const addSubTwo = vi.fn().mockImplementation((onChange: () => void) => {
+      subs.push(onChange);
+      return () => {
+        subs = subs.filter((s) => s !== onChange);
+      };
+    });
+    const addSub = { sub: addSubOne };
+    let storeValue = 0;
+    const setStoreValue = (val: number) => {
+      storeValue = val;
+      subs.forEach((s) => s());
+    };
+    const getStoreValue = () => storeValue;
+
+    const fn = hooks.hooked(() => {
+      const storeVal = hooks.useSyncExternalStore(addSub.sub, getStoreValue);
+
+      return storeVal + 1;
+    });
+
+    expect(fn()).toEqual(1);
+    expect(addSubOne).not.toHaveBeenCalled();
+    expect(subs.length).toEqual(0);
+    await delay(10);
+    expect(addSubOne).toHaveBeenCalledOnce();
+    expect(addSubTwo).not.toHaveBeenCalled();
+    expect(subs.length).toEqual(1);
+    expect(fn()).toEqual(1);
+    setStoreValue(1);
+    // value is updated right away even though rerender hasn't happened yet
+    expect(fn()).toEqual(2);
+    await delay(10);
+    expect(fn()).toEqual(2);
+
+    const oldsub = subs[0];
+    addSub.sub = addSubTwo;
+    expect(fn()).toEqual(2);
+    expect(subs.length).toEqual(1);
+    await delay(10);
+    expect(fn()).toEqual(2);
+    expect(subs.length).toEqual(1);
+    expect(subs[0]).not.toEqual(oldsub);
+    expect(addSubTwo).toHaveBeenCalledTimes(1);
+
+    setStoreValue(2);
+    await delay(10);
+    expect(fn()).toEqual(3);
+
+    hooks.dropEffect(fn);
+    expect(subs.length).toEqual(1);
+    await delay(10);
+    expect(subs.length).toEqual(0);
+  });
 });
