531bcf2ff01382cd8b613ca797bdb4b46f04e048
jaredvu <jaredvu@gmail.com>
2024-09-16 13:29:00 -0700

    feat(metadata-client): add client for metadata service

---
+++ src/lib/metadataService.ts
@@ -0,0 +1,93 @@
+type MetadataServiceInfoResponse = Record<
+  string,
+  {
+    name: string;
+    logo: string;
+    urls: {
+      website: string;
+      technical_doc: string;
+      cmc: string;
+    };
+    sector_tags: string[];
+    exchanges: string[];
+  }
+>;
+
+type MetadataServicePricesResponse = Record<
+  string,
+  {
+    price: number;
+    percent_change_24h: number;
+    volume_24h: number;
+    market_cap: number;
+  }
+>;
+
+type MetadataServiceCandlesResponse = Record<
+  string,
+  {
+    time: string; // ISO date string
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+  }
+>;
+
+/**
+ * MetadataServiceClient
+ * @description client to fetch metadata info, prices, and candles
+ */
+class MetadataServiceClient {
+  private host: string;
+
+  constructor(host: string) {
+    this.host = host;
+  }
+
+  _post(endpoint: string, params: {} = {}) {
+    return fetch(`${this.host}/${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    })
+      .then((res) => res.json())
+      .then((data) => data);
+  }
+
+  async getMarketmap(): Promise<Record<string, string>> {
+    return this._post('marketmap-map');
+  }
+
+  async getAssetInfo({ assets }: { assets?: string[] }): Promise<MetadataServiceInfoResponse> {
+    return this._post('info', {
+      assets,
+    });
+  }
+
+  async getAssetPrices({ assets }: { assets?: string[] }): Promise<MetadataServicePricesResponse> {
+    return this._post('prices', {
+      assets,
+    });
+  }
+
+  async getCandles({
+    assets,
+    resolution,
+  }: {
+    assets?: string[];
+    resolution: '1d' | '7d' | '30d';
+  }): Promise<MetadataServiceCandlesResponse> {
+    return this._post('candles', {
+      assets,
+      resolution,
+    });
+  }
+}
+
+const METADATA_URI: string = import.meta.env.VITE_METADATA_SERVICE_URI;
+const metadataClient = new MetadataServiceClient(METADATA_URI);
+export default metadataClient;
