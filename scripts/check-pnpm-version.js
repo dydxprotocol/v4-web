a0001f09b5931051f7efd0cff766735a5dce4359
yogurtandjam <jeremy@dydx.exchange>
2024-09-20 12:08:49 -0400

    chore: check pnpm version [OTE-820]

---
+++ scripts/check-pnpm-version.js
@@ -0,0 +1,22 @@
+// Get the user agent (package manager) that initiated the install
+const userAgent = process.env.npm_config_user_agent ?? '';
+console.log('userAgent', userAgent);
+
+// Check if pnpm was used
+if (!userAgent.includes('pnpm')) {
+  console.error('Error: This project requires pnpm. Please use pnpm to install dependencies.');
+  process.exit(1);
+}
+
+// Extract pnpm version from userAgent
+const userAgentParts = userAgent.split(' ');
+const pnpmInfo = userAgentParts.find((part) => part.startsWith('pnpm/'));
+const pnpmVersion = pnpmInfo ? pnpmInfo.split('/')[1] : '';
+const requiredVersion = '8.6.6'; // Change to your required version
+
+if (pnpmVersion !== requiredVersion) {
+  console.error(
+    `Error: This project requires pnpm version ${requiredVersion}, but you're using ${pnpmVersion}`
+  );
+  process.exit(1);
+}
