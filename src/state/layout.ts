d01fe8b85df667c281baf8ce48acc8fec1386669
mulan xia <mulanjxia@gmail.com>
2024-09-24 11:00:36 -0400

    add versioning

--- src/state/layout.ts
+++ src/state/layout.ts
@@ -1,19 +1,25 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 import { TradeLayouts } from '@/constants/layout';
-import { LocalStorageKey } from '@/constants/localStorage';
+import { LOCAL_STORAGE_VERSIONS, LocalStorageKey } from '@/constants/localStorage';
 
 import { getLocalStorage, setLocalStorage } from '@/lib/localStorage';
 
 export interface LayoutState {
-  selectedTradeLayout: TradeLayouts;
+  selectedTradeLayout: {
+    version: string;
+    layout: TradeLayouts;
+  };
   isSidebarOpen: boolean;
 }
 
 const initialState = {
   selectedTradeLayout: getLocalStorage({
     key: LocalStorageKey.SelectedTradeLayout,
-    defaultValue: TradeLayouts.Default,
+    defaultValue: {
+      version: LOCAL_STORAGE_VERSIONS[LocalStorageKey.SelectedTradeLayout],
+      layout: TradeLayouts.Default,
+    },
   }),
   isSidebarOpen: true,
 };
@@ -23,8 +29,17 @@ export const layoutSlice = createSlice({
   initialState,
   reducers: {
     setSelectedTradeLayout: (state: LayoutState, { payload }: PayloadAction<TradeLayouts>) => {
-      setLocalStorage({ key: LocalStorageKey.SelectedTradeLayout, value: payload });
-      state.selectedTradeLayout = payload;
+      setLocalStorage({
+        key: LocalStorageKey.SelectedTradeLayout,
+        value: {
+          version: LOCAL_STORAGE_VERSIONS[LocalStorageKey.SelectedTradeLayout],
+          layout: payload,
+        },
+      });
+      state.selectedTradeLayout = {
+        version: LOCAL_STORAGE_VERSIONS[LocalStorageKey.SelectedTradeLayout],
+        layout: payload,
+      };
     },
     setIsSidebarOpen: (state: LayoutState, { payload }: PayloadAction<boolean>) => ({
       ...state,
--- src/state/layoutSelectors.ts
+++ src/state/layoutSelectors.ts
@@ -1,6 +1,8 @@
 import { TradeLayouts } from '@/constants/layout';
+import { LOCAL_STORAGE_VERSIONS, LocalStorageKey } from '@/constants/localStorage';
 
-import type { RootState } from './_store';
+import { store, type RootState } from './_store';
+import { setSelectedTradeLayout } from './layout';
 
 /**
  * @param state
@@ -10,7 +12,15 @@ export const getIsSidebarOpen = (state: RootState) => state.layout.isSidebarOpen
 
 /**
  * @param state
- * @returns Currently selected TradeLayout
+ * @returns Currently selected TradeLayout and version
  */
-export const getSelectedTradeLayout = (state: RootState): TradeLayouts =>
-  state.layout.selectedTradeLayout;
+export const getSelectedTradeLayout = (state: RootState): TradeLayouts => {
+  const { version, layout } = state.layout.selectedTradeLayout;
+
+  // Ensure version matches, otherwise wipe
+  if (version !== LOCAL_STORAGE_VERSIONS[LocalStorageKey.SelectedTradeLayout]) {
+    store.dispatch(setSelectedTradeLayout(TradeLayouts.Default));
+    return TradeLayouts.Default;
+  }
+  return layout;
+};
