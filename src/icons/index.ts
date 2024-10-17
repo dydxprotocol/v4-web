0803b1bed918ad35a708b3a97497d60ad6383802
moo-onthelawn <70078372+moo-onthelawn@users.noreply.github.com>
2024-10-17 12:53:25 -0400

    feat(uirefresh): component refresh - horizontal panel filter toggle (#1161)

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -65,6 +65,7 @@ export { default as RocketIcon } from './rocket.svg';
 export { default as RoundedArrowIcon } from './rounded-arrow.svg';
 export { default as SearchIcon } from './search.svg';
 export { default as SendIcon } from './send.svg';
+export { default as SettingsIcon } from './settings.svg';
 export { default as ShareIcon } from './share.svg';
 export { default as ShowIcon } from './show.svg';
 export { default as SocialXIcon } from './social-x.svg';
