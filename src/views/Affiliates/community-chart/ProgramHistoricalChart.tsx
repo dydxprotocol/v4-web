748e93dc0861de06145887ab4f8a649785ce9482
Bill He <bill@dydx.exchange>
2024-10-18 15:13:33 -0700

    fix bugs

--- src/views/Affiliates/community-chart/ProgramHistoricalChart.tsx
+++ src/views/Affiliates/community-chart/ProgramHistoricalChart.tsx
@@ -2,7 +2,6 @@ import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { curveLinear } from '@visx/curve';
 import { TooltipContextType } from '@visx/xychart';
-import { debounce } from 'lodash';
 import styled from 'styled-components';
 
 import {
@@ -11,7 +10,6 @@ import {
   affiliatesProgramPeriods,
   type TradingRewardsDatum,
 } from '@/constants/charts';
-import { NORMAL_DEBOUNCE_MS } from '@/constants/debounce';
 import { STRING_KEYS } from '@/constants/localization';
 import { TOKEN_DECIMALS } from '@/constants/numbers';
 import { timeUnits } from '@/constants/time';
@@ -108,28 +106,6 @@ export const ProgramHistoricalChart = ({
     [now, historyStartDate, newestDataPointDate, oldestDataPointDate]
   );
 
-  // Update selected period in toggle if user zooms in/out
-  const onZoomSnap = useMemo(
-    () =>
-      debounce(({ zoomDomain }: { zoomDomain?: number }) => {
-        if (zoomDomain) {
-          const predefinedPeriodIx = affiliatesProgramPeriods.findIndex(
-            // To account for slight variance from zoom animation
-            (period) => Math.abs(msForPeriod(period) - zoomDomain) <= 1
-          );
-          if (predefinedPeriodIx < 0) {
-            // Unselect period
-            setIsZooming(true);
-          } else {
-            // Update period to new selected period
-            setIsZooming(false);
-            setSelectedPeriod(affiliatesProgramPeriods[predefinedPeriodIx]);
-          }
-        }
-      }, NORMAL_DEBOUNCE_MS),
-    [msForPeriod]
-  );
-
   useEffect(() => {
     if (isZooming) {
       setDefaultZoomDomain(undefined);
@@ -206,7 +182,6 @@ export const ProgramHistoricalChart = ({
         tickFormatY={tickFormatY}
         renderTooltip={renderTooltip}
         onTooltipContext={setTooltipContext}
-        onZoom={onZoomSnap}
         slotEmpty={slotEmpty}
         defaultZoomDomain={defaultZoomDomain}
         minZoomDomain={PROGRAM_DATA_TIME_RESOLUTION * 2}
