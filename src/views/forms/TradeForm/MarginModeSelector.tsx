31ff05cb77427dcf14aa22ae6ac919150a1f56b5
moo-onthelawn <70078372+moo-onthelawn@users.noreply.github.com>
2024-10-25 14:55:37 -0400

    feat(uirefresh): component refresh - trade box [2/2] (#1214)

--- src/views/forms/TradeForm/MarginModeSelector.tsx
+++ src/views/forms/TradeForm/MarginModeSelector.tsx
@@ -3,25 +3,28 @@ import { useCallback } from 'react';
 import { shallowEqual } from 'react-redux';
 import styled from 'styled-components';
 
-import { MARGIN_MODE_STRINGS } from '@/constants/abacus';
+import { AbacusMarginMode, MARGIN_MODE_STRINGS, TradeInputField } from '@/constants/abacus';
 import { DialogTypes, TradeBoxDialogTypes } from '@/constants/dialogs';
 import { STRING_KEYS } from '@/constants/localization';
 
 import { useStringGetter } from '@/hooks/useStringGetter';
 
+import breakpoints from '@/styles/breakpoints';
 import { layoutMixins } from '@/styles/layoutMixins';
 
 import { Button } from '@/components/Button';
 import { Icon, IconName } from '@/components/Icon';
+import { ToggleGroup } from '@/components/ToggleGroup';
 import { WithTooltip } from '@/components/WithTooltip';
 
 import { calculateCanAccountTrade } from '@/state/accountCalculators';
 import { useAppDispatch, useAppSelector } from '@/state/appTypes';
 import { openDialog, openDialogInTradeBox } from '@/state/dialogs';
-import { getInputTradeData, useTradeFormData } from '@/state/inputsSelectors';
+import { getInputTradeMarginMode, useTradeFormData } from '@/state/inputsSelectors';
 import { getCurrentMarketAssetId } from '@/state/perpetualsSelectors';
 
-import { orEmptyObj } from '@/lib/typeUtils';
+import abacusStateManager from '@/lib/abacus';
+import { testFlags } from '@/lib/testFlags';
 
 export const MarginModeSelector = ({
   className,
@@ -30,13 +33,18 @@ export const MarginModeSelector = ({
   className?: string;
   openInTradeBox: boolean;
 }) => {
+  const dispatch = useAppDispatch();
   const stringGetter = useStringGetter();
+
   const currentAssetId = useAppSelector(getCurrentMarketAssetId);
-  const { marginMode } = orEmptyObj(useAppSelector(getInputTradeData, shallowEqual));
-  const { needsMarginMode } = useTradeFormData();
   const canAccountTrade = useAppSelector(calculateCanAccountTrade);
 
-  const dispatch = useAppDispatch();
+  const marginMode =
+    useAppSelector(getInputTradeMarginMode, shallowEqual) ?? AbacusMarginMode.Cross;
+  const { needsMarginMode } = useTradeFormData();
+
+  const { uiRefresh } = testFlags;
+
   const handleClick = useCallback(() => {
     dispatch(
       openInTradeBox
@@ -45,42 +53,119 @@ export const MarginModeSelector = ({
     );
   }, [dispatch, openInTradeBox]);
 
+  const setMarginMode = (value: string) => {
+    abacusStateManager.setTradeValue({
+      value,
+      field: TradeInputField.marginMode,
+    });
+  };
+
+  const warningTooltip = (
+    <div tw="flex flex-row [align-items:start]">
+      <Icon iconName={IconName.Warning} tw="text-[1.5rem] text-color-warning" />
+      {stringGetter({
+        key: STRING_KEYS.UNABLE_TO_CHANGE_MARGIN_MODE,
+        params: {
+          MARKET: currentAssetId,
+        },
+      })}
+    </div>
+  );
+
+  if (uiRefresh) {
+    const showMarginModeUnToggleableTooltip = canAccountTrade && !needsMarginMode;
+
+    const selector = (
+      <$MarginModeSelector tw="flex flex-1 items-center justify-between">
+        {stringGetter({
+          key: STRING_KEYS.MARGIN_MODE,
+        })}
+        <$ToggleGroup
+          disabled={!canAccountTrade || !needsMarginMode}
+          withSeparators
+          truncateLabel={false}
+          items={[
+            {
+              value: AbacusMarginMode.Cross.rawValue,
+              label: showMarginModeUnToggleableTooltip ? (
+                stringGetter({
+                  key: MARGIN_MODE_STRINGS[AbacusMarginMode.Cross.rawValue]!,
+                })
+              ) : (
+                <WithTooltip
+                  side="left"
+                  tooltipString={stringGetter({ key: STRING_KEYS.CROSS_MARGIN_DESCRIPTION })}
+                >
+                  {stringGetter({
+                    key: MARGIN_MODE_STRINGS[AbacusMarginMode.Cross.rawValue]!,
+                  })}
+                </WithTooltip>
+              ),
+              disabled: !needsMarginMode && marginMode !== AbacusMarginMode.Cross,
+            },
+            {
+              value: AbacusMarginMode.Isolated.rawValue,
+              label: showMarginModeUnToggleableTooltip ? (
+                stringGetter({
+                  key: MARGIN_MODE_STRINGS[AbacusMarginMode.Isolated.rawValue]!,
+                })
+              ) : (
+                <WithTooltip
+                  align="end"
+                  tooltipString={stringGetter({ key: STRING_KEYS.ISOLATED_MARGIN_DESCRIPTION })}
+                >
+                  {stringGetter({
+                    key: MARGIN_MODE_STRINGS[AbacusMarginMode.Isolated.rawValue]!,
+                  })}
+                </WithTooltip>
+              ),
+              disabled: !needsMarginMode && marginMode !== AbacusMarginMode.Isolated,
+            },
+          ]}
+          value={marginMode.rawValue}
+          onValueChange={setMarginMode}
+        />
+      </$MarginModeSelector>
+    );
+
+    return showMarginModeUnToggleableTooltip ? (
+      <$WarningTooltip className={className} slotTooltip={warningTooltip}>
+        {selector}
+      </$WarningTooltip>
+    ) : (
+      selector
+    );
+  }
+
   return needsMarginMode ? (
     <Button onClick={handleClick} className={className} disabled={!canAccountTrade}>
       <$Text>
-        {marginMode &&
-          stringGetter({
-            key: MARGIN_MODE_STRINGS[marginMode.rawValue]!,
-          })}
+        {stringGetter({
+          key: MARGIN_MODE_STRINGS[marginMode.rawValue]!,
+        })}
       </$Text>
       <Icon iconName={IconName.Triangle} tw="ml-[0.5ch] rotate-[0.75turn] text-[0.4375rem]" />
     </Button>
   ) : (
-    <$WarningTooltip
-      className={className}
-      slotTooltip={
-        <div tw="flex flex-row [align-items:start]">
-          <Icon iconName={IconName.Warning} tw="text-[1.5rem] text-color-warning" />
-          {stringGetter({
-            key: STRING_KEYS.UNABLE_TO_CHANGE_MARGIN_MODE,
-            params: {
-              MARKET: currentAssetId,
-            },
-          })}
-        </div>
-      }
-    >
+    <$WarningTooltip className={className} slotTooltip={warningTooltip}>
       <Button disabled>
         <$Text>
-          {marginMode &&
-            stringGetter({
-              key: MARGIN_MODE_STRINGS[marginMode.rawValue]!,
-            })}
+          {stringGetter({
+            key: MARGIN_MODE_STRINGS[marginMode.rawValue]!,
+          })}
         </$Text>
       </Button>
     </$WarningTooltip>
   );
 };
+
+const $MarginModeSelector = styled.div`
+  font: var(--font-mini-book);
+  @media ${breakpoints.tablet} {
+    font: var(--font-small-book);
+  }
+`;
+
 const $WarningTooltip = styled(WithTooltip)`
   --tooltip-backgroundColor: var(--color-gradient-warning);
   border: 1px solid ${({ theme }) => theme.warning}30;
@@ -89,4 +174,10 @@ const $WarningTooltip = styled(WithTooltip)`
 
 const $Text = styled.div`
   ${layoutMixins.textTruncate};
+`;
+
+const $ToggleGroup = styled(ToggleGroup)`
+  --separator-padding: 1rem;
+  flex-basis: 100%;
+  max-width: max-content;
 `;
