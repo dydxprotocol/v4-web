826b4723a2c91ff0f1686104da938c680734790a
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-11-01 19:15:31 -0400

    revert 1131

--- src/views/charts/TradingView/TvChart.tsx
+++ src/views/charts/TradingView/TvChart.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
-import BigNumber from 'bignumber.js';
 import type { ResolutionString } from 'public/tradingview/charting_library';
 
 import { DEFAULT_MARKETID } from '@/constants/markets';
@@ -11,26 +10,17 @@ import { useChartLines } from '@/hooks/tradingView/useChartLines';
 import { useChartMarketAndResolution } from '@/hooks/tradingView/useChartMarketAndResolution';
 import { useOrderbookCandles } from '@/hooks/tradingView/useOrderbookCandles';
 import { useTradingView } from '@/hooks/tradingView/useTradingView';
-import { useTradingViewDatafeed } from '@/hooks/tradingView/useTradingViewDatafeed';
 import { useTradingViewTheme } from '@/hooks/tradingView/useTradingViewTheme';
 import { useTradingViewToggles } from '@/hooks/tradingView/useTradingViewToggles';
-import { useDydxClient } from '@/hooks/useDydxClient';
-import usePrevious from '@/hooks/usePrevious';
 
 import { useAppSelector } from '@/state/appTypes';
-import { getSelectedDisplayUnit } from '@/state/appUiConfigsSelectors';
-import { getCurrentMarketConfig, getCurrentMarketId } from '@/state/perpetualsSelectors';
-
-import { orEmptyObj } from '@/lib/typeUtils';
+import { getCurrentMarketId } from '@/state/perpetualsSelectors';
 
 import { BaseTvChart } from './BaseTvChart';
 
 export const TvChart = () => {
   const [isChartReady, setIsChartReady] = useState(false);
   const currentMarketId: string = useAppSelector(getCurrentMarketId) ?? DEFAULT_MARKETID;
-  const { tickSizeDecimals: tickSizeDecimalsAbacus } = orEmptyObj(
-    useAppSelector(getCurrentMarketConfig)
-  );
 
   const tvWidgetRef = useRef<TvWidget | null>(null);
   const tvWidget = tvWidgetRef.current;
@@ -44,31 +34,6 @@ export const TvChart = () => {
   const buySellMarksToggleRef = useRef<HTMLElement | null>(null);
   const buySellMarksToggle = buySellMarksToggleRef.current;
 
-  const [tickSizeDecimalsIndexer, setTickSizeDecimalsIndexer] = useState<{
-    [marketId: string]: number | undefined;
-  }>({});
-
-  const tickSizeDecimals =
-    (currentMarketId
-      ? tickSizeDecimalsIndexer[currentMarketId] ?? tickSizeDecimalsAbacus
-      : tickSizeDecimalsAbacus) ?? undefined;
-
-  const { getMarketTickSize } = useDydxClient();
-
-  useEffect(() => {
-    // we only need tick size from current market for the price scale settings
-    // if markets haven't been loaded via abacus, get the current market info from indexer
-    (async () => {
-      if (currentMarketId && tickSizeDecimals === undefined) {
-        const marketTickSize = await getMarketTickSize(currentMarketId);
-        setTickSizeDecimalsIndexer((prev) => ({
-          ...prev,
-          [currentMarketId]: BigNumber(marketTickSize).decimalPlaces() ?? undefined,
-        }));
-      }
-    })();
-  }, [currentMarketId, getMarketTickSize, tickSizeDecimals]);
-
   const {
     orderLinesToggleOn,
     setOrderLinesToggleOn,
@@ -77,12 +42,6 @@ export const TvChart = () => {
     setBuySellMarksToggleOn,
     buySellMarksToggleOn,
   } = useTradingViewToggles();
-
-  const { datafeed, resetCache } = useTradingViewDatafeed(
-    orderbookCandlesToggleOn,
-    tickSizeDecimals
-  );
-
   const { savedResolution } = useTradingView({
     tvWidgetRef,
     orderLineToggleRef,
@@ -95,8 +54,6 @@ export const TvChart = () => {
     buySellMarksToggleOn,
     setBuySellMarksToggleOn,
     setIsChartReady,
-    tickSizeDecimals,
-    datafeed,
   });
   useChartMarketAndResolution({
     currentMarketId,
@@ -123,22 +80,6 @@ export const TvChart = () => {
     isChartReady,
   });
   useTradingViewTheme({ tvWidget, isWidgetReady, chartLines });
-
-  const displayUnit = useAppSelector(getSelectedDisplayUnit);
-  const prevDisplayUnit = usePrevious(displayUnit);
-
-  useEffect(() => {
-    if (!isChartReady || !tvWidget) return;
-
-    // Only reset data if displayUnit has actually changed
-    if (prevDisplayUnit !== displayUnit) {
-      const chart = tvWidget.activeChart();
-      if (chart) {
-        chart.resetData();
-        resetCache();
-      }
-    }
-  }, [displayUnit, tvWidget, isChartReady, prevDisplayUnit, resetCache]);
 
   return <BaseTvChart isChartReady={isChartReady} />;
 };
