87c5860463cecb34af523e89d4d8eb437b1febae
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-11-05 13:00:18 -0500

    restore newline changes

--- src/views/charts/TradingView/BaseTvChart.tsx
+++ src/views/charts/TradingView/BaseTvChart.tsx
@@ -13,9 +13,7 @@ export const BaseTvChart = ({ tvWidget }: { tvWidget?: TvWidget | null }) => {
 
   useEffect(() => {
     setIsChartReady(false);
-    tvWidget?.onChartReady(() => {
-      setIsChartReady(true);
-    });
+    tvWidget?.onChartReady(() => setIsChartReady(true));
   }, [tvWidget]);
 
   return (
