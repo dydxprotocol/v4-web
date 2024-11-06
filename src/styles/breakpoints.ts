fd0201af1f6cd4a9a74bce9caf45e8dab5fc78e6
tyleroooo <tyler@dydx.exchange>
2024-11-06 14:56:41 -0500

    fix: nicer tables in a few ways (#1254)

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -1,10 +1,10 @@
 import { BREAKPOINT_REM } from '@/constants/page';
 
 export default {
-  mobile: `(max-width: ${BREAKPOINT_REM.mobile})`,
-  notMobile: `(min-width: ${BREAKPOINT_REM.mobile})`,
-  tablet: `(max-width: ${BREAKPOINT_REM.tablet})`,
-  notTablet: `(min-width: ${BREAKPOINT_REM.tablet})`,
+  mobile: `(width <= ${BREAKPOINT_REM.mobile})`,
+  notMobile: `(width > ${BREAKPOINT_REM.mobile})`,
+  tablet: `(width <= ${BREAKPOINT_REM.tablet})`,
+  notTablet: `(width > ${BREAKPOINT_REM.tablet})`,
   desktopSmall: `(max-width: ${BREAKPOINT_REM.desktopSmall})`,
   desktopMedium: `(min-width: ${BREAKPOINT_REM.desktopMedium})`,
   desktopLarge: `(min-width: ${BREAKPOINT_REM.desktopLarge})`,
