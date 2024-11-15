95444bf5270c5d9b95da8daacffea3a8e2209c30
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-11-15 09:57:14 +0100

    Fix Google Tag Manager parameter injection

--- scripts/inject-google-tag-manager.js
+++ scripts/inject-google-tag-manager.js
@@ -28,7 +28,7 @@ async function inject(fileName) {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-ABCDEFGH');</script>
+        })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_CONTAINER_ID}');</script>
         <!-- End Google Tag Manager -->
   `;
   const injectedHtml = html.replace('</head>', `${googleTagManagerHeadScript}\n</head>`);
