b6cee7ade4388e1aba3fcf9f6e0346644c48a9a8
jaredvu <jaredvu@gmail.com>
2024-11-26 16:16:42 -0800

    remove test var

--- src/hooks/useComplianceState.tsx
+++ src/hooks/useComplianceState.tsx
@@ -112,6 +112,6 @@ export const useComplianceState = () => {
     complianceState,
     complianceMessage,
     disableConnectButton,
-    showRestrictionWarning: true, // complianceState === ComplianceStates.READ_ONLY,
+    showRestrictionWarning: complianceState === ComplianceStates.READ_ONLY,
   };
 };
