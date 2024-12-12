d481e688e20a27068ec9fb7252f29d201d8f166a
jaredvu <jaredvu@gmail.com>
2024-12-12 10:00:32 -0800

    priceChangePercent

--- src/lib/calculator/markets.ts
+++ src/lib/calculator/markets.ts
@@ -1,11 +1,14 @@
-import { getFractionDigits } from '../numbers';
+import { PERCENT_DECIMALS } from '@/constants/numbers';
+
+import { isTruthy } from '../isTruthy';
+import { BigNumberish, getFractionDigits, MustBigNumber } from '../numbers';
 
 /**
  *
  * @param tickSize perpetual market's tick size
  * @returns number of decimal places in the tickSize
  */
-export const getTickSizeDecimals = (tickSize: string): number => {
+export const getTickSizeDecimals = (tickSize: BigNumberish): number => {
   return getFractionDigits(tickSize);
 };
 
@@ -14,6 +17,26 @@ export const getTickSizeDecimals = (tickSize: string): number => {
  * @param stepSize perpetual market's tick size
  * @returns number of decimal places in the tickSize
  */
-export const getStepSizeDecimals = (stepSize: string): number => {
+export const getStepSizeDecimals = (stepSize: BigNumberish): number => {
   return getFractionDigits(stepSize);
+};
+
+/**
+ *
+ * @param priceChange24H perpetual market's 24h price change
+ * @param oraclePrice perpetual market's oracle price
+ * @returns percent change in the perpetual market's price over the last 24 hours
+ */
+export const get24hPriceChangePercent = ({
+  priceChange24H,
+  oraclePrice,
+}: {
+  priceChange24H: BigNumberish;
+  oraclePrice: BigNumberish;
+}): string | undefined => {
+  if (isTruthy(oraclePrice)) {
+    return MustBigNumber(priceChange24H).div(oraclePrice).toFixed(PERCENT_DECIMALS);
+  }
+
+  return undefined;
 };
