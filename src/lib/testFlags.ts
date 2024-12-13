e4a500356105ac9586aeeaaeb75a48d10a739709
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-12-13 12:57:22 -0500

    add placeholder for new deposit flow

--- src/lib/testFlags.ts
+++ src/lib/testFlags.ts
@@ -52,12 +52,12 @@ class TestFlags {
     return this.queryParams.ref;
   }
 
-  get onboardingRewrite() {
-    return !!this.queryParams.onboarding_rewrite;
-  }
-
   get showInstantDepositToggle() {
     return !!this.queryParams.funkit_toggle;
+  }
+
+  get showNewDepositFlow() {
+    return !!this.queryParams.deposit_rewrite;
   }
 }
 
