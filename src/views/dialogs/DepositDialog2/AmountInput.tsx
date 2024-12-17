882afc57061c947f7cfa3ec3d4e66914ddc45830
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-12-16 21:58:25 -0500

    add token amount input box

---
+++ src/views/dialogs/DepositDialog2/AmountInput.tsx
@@ -0,0 +1,67 @@
+import { EventHandler } from 'react';
+
+import { SyntheticInputEvent } from 'react-number-format/types/types';
+import styled from 'styled-components';
+
+import { STRING_KEYS } from '@/constants/localization';
+
+import { useStringGetter } from '@/hooks/useStringGetter';
+
+import { AssetIcon } from '@/components/AssetIcon';
+import { Icon, IconName } from '@/components/Icon';
+
+export type AmountInputProps = {
+  value: string;
+  onChange: (newValue: string) => void;
+  token: { chain: string; address: string; symbol: string };
+  onTokenClick: () => void;
+};
+
+const numericValueRegex = /^\d*(?:\\[.])?\d*$/;
+function escapeRegExp(string: string): string {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+export const AmountInput = ({ value, onChange, token, onTokenClick }: AmountInputProps) => {
+  const stringGetter = useStringGetter();
+
+  const onValueChange: EventHandler<SyntheticInputEvent> = (e) => {
+    if (!numericValueRegex.test(escapeRegExp(e.target.value))) {
+      return;
+    }
+
+    onChange(e.target.value);
+  };
+
+  return (
+    <div tw="flex items-center justify-between gap-1 rounded-0.75 border border-solid border-color-border bg-color-layer-4 px-1.25 py-0.75">
+      <div tw="flex flex-col gap-0.5 text-small">
+        <div>{stringGetter({ key: STRING_KEYS.AMOUNT })}</div>
+        <input
+          type="number"
+          placeholder="0.00"
+          tw="flex-1 bg-color-layer-4 text-large font-medium outline-none"
+          value={value}
+          onChange={onValueChange}
+        />
+      </div>
+      <button
+        tw="flex items-center gap-0.75 rounded-0.75 border border-solid border-color-layer-6 bg-color-layer-5 px-0.5 py-0.375"
+        type="button"
+        onClick={onTokenClick}
+      >
+        <div tw="flex items-center gap-0.5">
+          {/* TODO(deposit2.0): Also include chain logo */}
+          <AssetIcon tw="h-[2rem] w-[2rem]" symbol={token.symbol} />
+          <div>{token.symbol}</div>
+        </div>
+        <$CaretIcon size="10px" iconName={IconName.Caret} />
+      </button>
+    </div>
+  );
+};
+
+const $CaretIcon = styled(Icon)`
+  transform: rotate(-90deg);
+  color: var(--color-text-0);
+`;
--- src/views/dialogs/DepositDialog2/DepositDialog2.tsx
+++ src/views/dialogs/DepositDialog2/DepositDialog2.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+
+import styled from 'styled-components';
+
 import { DepositDialog2Props, DialogProps } from '@/constants/dialogs';
 import { STRING_KEYS } from '@/constants/localization';
 
@@ -6,19 +10,45 @@ import { useStringGetter } from '@/hooks/useStringGetter';
 
 import { Dialog, DialogPlacement } from '@/components/Dialog';
 
+import { DepositForm } from './DepositForm';
+import { TokenSelect } from './TokenSelect';
+
 export const DepositDialog2 = ({ setIsOpen }: DialogProps<DepositDialog2Props>) => {
   const { isMobile } = useBreakpoints();
   const stringGetter = useStringGetter();
 
+  const [formState, setFormState] = useState<'form' | 'token-select'>('form');
+  // TODO(deposit2): localization
+  const dialogTitle =
+    formState === 'form' ? stringGetter({ key: STRING_KEYS.DEPOSIT }) : 'Select Token';
+
   return (
-    <Dialog
+    <$Dialog
       isOpen
+      withAnimation
       hasHeaderBorder
       setIsOpen={setIsOpen}
-      title={stringGetter({ key: STRING_KEYS.DEPOSIT })}
+      title={dialogTitle}
       placement={isMobile ? DialogPlacement.FullScreen : DialogPlacement.Default}
     >
-      New deposit flow goes here!
-    </Dialog>
+      <div tw="flex w-[200%] overflow-hidden">
+        <div
+          tw="w-[50%]"
+          style={{ marginLeft: formState === 'form' ? 0 : '-50%', transition: 'margin 350ms' }}
+        >
+          <DepositForm onTokenSelect={() => setFormState('token-select')} />
+        </div>
+        <div tw="w-[50%]">
+          <TokenSelect onBack={() => setFormState('form')} />
+        </div>
+      </div>
+    </$Dialog>
   );
 };
+
+const $Dialog = styled(Dialog)`
+  --dialog-content-paddingTop: 0;
+  --dialog-content-paddingRight: 0;
+  --dialog-content-paddingBottom: 0;
+  --dialog-content-paddingLeft: 0;
+`;
