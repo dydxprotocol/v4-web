931d2ea9f1cf9a3540a95aebbbc30975fbbdb337
Tyler <tyler@dydx.exchange>
2024-12-20 11:47:24 -0500

    undo-ui-facing-abacus-ts

--- src/views/tables/OrdersTable.tsx
+++ src/views/tables/OrdersTable.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, Key, ReactNode, useMemo } from 'react';
+import { forwardRef, Key, ReactNode, useEffect, useMemo } from 'react';
 
 import { OrderSide } from '@dydxprotocol/v4-client-js';
 import { ColumnSize } from '@react-types/table';
@@ -14,7 +14,6 @@ import { TOKEN_DECIMALS } from '@/constants/numbers';
 import { EMPTY_ARR } from '@/constants/objects';
 
 import { useBreakpoints } from '@/hooks/useBreakpoints';
-import { useViewPanel } from '@/hooks/useSeen';
 import { useStringGetter } from '@/hooks/useStringGetter';
 
 import breakpoints from '@/styles/breakpoints';
@@ -34,8 +33,13 @@ import { Tag, TagSize } from '@/components/Tag';
 import { WithTooltip } from '@/components/WithTooltip';
 import { MarketTypeFilter, marketTypeMatchesFilter } from '@/pages/trade/types';
 
+import { viewedOrders } from '@/state/account';
 import { calculateIsAccountViewOnly } from '@/state/accountCalculators';
-import { getCurrentMarketOrders, getSubaccountUnclearedOrders } from '@/state/accountSelectors';
+import {
+  getCurrentMarketOrders,
+  getHasUnseenOrderUpdates,
+  getSubaccountUnclearedOrders,
+} from '@/state/accountSelectors';
 import { useAppDispatch, useAppSelector } from '@/state/appTypes';
 import { getAssets } from '@/state/assetsSelectors';
 import { openDialog } from '@/state/dialogs';
@@ -432,7 +436,11 @@ export const OrdersTable = forwardRef(
     const allPerpetualMarkets = orEmptyRecord(useAppSelector(getPerpetualMarkets, shallowEqual));
     const allAssets = orEmptyRecord(useAppSelector(getAssets, shallowEqual));
 
-    useViewPanel(currentMarket, 'openOrders');
+    const hasUnseenOrderUpdates = useAppSelector(getHasUnseenOrderUpdates);
+
+    useEffect(() => {
+      if (hasUnseenOrderUpdates) dispatch(viewedOrders());
+    }, [hasUnseenOrderUpdates]);
 
     const symbol = mapIfPresent(currentMarket, (market) =>
       mapIfPresent(allPerpetualMarkets[market]?.assetId, (assetId) => allAssets[assetId]?.id)
