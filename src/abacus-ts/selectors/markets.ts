f6c6b538030227ef0d37e6a605f0a05032c8ad1d
Jared Vu <jaredvu@gmail.com>
2025-01-06 13:36:44 -0800

    feat(bonsai-core): migrate market header (#1398)

--- src/abacus-ts/selectors/markets.ts
+++ src/abacus-ts/selectors/markets.ts
@@ -2,7 +2,8 @@ import { createAppSelector } from '@/state/appTypes';
 import { getCurrentMarketId } from '@/state/perpetualsSelectors';
 
 import { calculateAllMarkets } from '../calculators/markets';
-import { selectRawMarketsData } from './base';
+import { mergeLoadableStatus } from '../lib/mapLoadable';
+import { selectRawMarkets, selectRawMarketsData } from './base';
 
 export const selectAllMarketsInfo = createAppSelector([selectRawMarketsData], (markets) =>
   calculateAllMarkets(markets)
@@ -12,3 +13,5 @@ export const selectCurrentMarketInfo = createAppSelector(
   [selectAllMarketsInfo, getCurrentMarketId],
   (markets, currentMarketId) => (currentMarketId ? markets?.[currentMarketId] : undefined)
 );
+
+export const selectMarketsInfoLoading = createAppSelector([selectRawMarkets], mergeLoadableStatus);
