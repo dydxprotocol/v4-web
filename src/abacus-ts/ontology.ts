78da1b5a9083dcf67b4f64e74fee17a3ee47b65f
jaredvu <jaredvu@gmail.com>
2025-01-07 16:48:59 -0800

    remove console log

--- src/abacus-ts/ontology.ts
+++ src/abacus-ts/ontology.ts
@@ -100,6 +100,6 @@ export const BonsaiHelpers = {
 } as const satisfies NestedSelectors;
 
 export const BonsaiHooks = {
-  useCurrentMarketHistoricalFunding,
+  // useCurrentMarketHistoricalFunding,
   useCurrentMarketLiveTrades: useCurrentMarketTradesValue,
 } as const;
