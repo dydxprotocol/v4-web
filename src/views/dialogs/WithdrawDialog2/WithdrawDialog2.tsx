43eacc32cee359df206efc73636c2932fd6c85e5
jaredvu <jaredvu@gmail.com>
2025-01-08 09:52:35 -0800

    update componentry for withdraws

--- src/views/dialogs/WithdrawDialog2/WithdrawDialog2.tsx
+++ src/views/dialogs/WithdrawDialog2/WithdrawDialog2.tsx
@@ -1,73 +1,37 @@
 import { useRef, useState } from 'react';
 
 import styled from 'styled-components';
-import { mainnet } from 'viem/chains';
 
 import { DepositDialog2Props, DialogProps } from '@/constants/dialogs';
-import { CosmosChainId } from '@/constants/graz';
 import { STRING_KEYS } from '@/constants/localization';
-import { SOLANA_MAINNET_ID } from '@/constants/solana';
-import { USDC_ADDRESSES, USDC_DECIMALS } from '@/constants/tokens';
-import { WalletNetworkType } from '@/constants/wallets';
 
-import { useAccounts } from '@/hooks/useAccounts';
 import { useBreakpoints } from '@/hooks/useBreakpoints';
 import { useStringGetter } from '@/hooks/useStringGetter';
 
 import { Dialog, DialogPlacement } from '@/components/Dialog';
 
-import { SourceAccount } from '@/state/wallet';
-
-import { DepositForm } from '../DepositDialog2/DepositForm';
-import { TokenSelect } from '../DepositDialog2/TokenSelect';
-import { DepositToken } from '../DepositDialog2/types';
-
-function getDefaultToken(sourceAccount: SourceAccount): DepositToken {
-  if (!sourceAccount.chain) throw new Error('No user chain detected');
-
-  // TODO(deposit2.0): Use user's biggest balance as the default token
-  if (sourceAccount.chain === WalletNetworkType.Evm) {
-    return {
-      chainId: mainnet.id.toString(),
-      denom: USDC_ADDRESSES[mainnet.id],
-      decimals: USDC_DECIMALS,
-    };
-  }
-
-  if (sourceAccount.chain === WalletNetworkType.Solana) {
-    return {
-      chainId: SOLANA_MAINNET_ID,
-      denom: USDC_ADDRESSES[SOLANA_MAINNET_ID],
-      decimals: USDC_DECIMALS,
-    };
-  }
-
-  return {
-    chainId: CosmosChainId.Osmosis,
-    denom: USDC_ADDRESSES[CosmosChainId.Osmosis],
-    decimals: USDC_DECIMALS,
-  };
-}
+import { ChainSelect } from './ChainSelect';
+import { WithdrawForm } from './WithdrawForm';
 
 export const WithdrawDialog2 = ({ setIsOpen }: DialogProps<DepositDialog2Props>) => {
-  const { sourceAccount } = useAccounts();
-
+  const [destinationAddress, setDestinationAddress] = useState('');
+  const [destinationChain, setDestinationChain] = useState('');
   const [amount, setAmount] = useState('');
-  const [token, setToken] = useState<DepositToken>(getDefaultToken(sourceAccount));
 
   const { isMobile } = useBreakpoints();
   const stringGetter = useStringGetter();
 
-  const [formState, setFormState] = useState<'form' | 'token-select'>('form');
-  const tokenSelectRef = useRef<HTMLDivElement | null>(null);
+  const [formState, setFormState] = useState<'form' | 'chain-select'>('form');
+  const chainSelectRef = useRef<HTMLDivElement | null>(null);
 
-  // TODO(deposit2): localization
   const dialogTitle =
-    formState === 'form' ? stringGetter({ key: STRING_KEYS.DEPOSIT }) : 'Select Token';
+    formState === 'form'
+      ? stringGetter({ key: STRING_KEYS.WITHDRAW })
+      : stringGetter({ key: STRING_KEYS.SELECT_CHAIN });
 
   const onShowForm = () => {
     setFormState('form');
-    tokenSelectRef.current?.scroll({ top: 0 });
+    chainSelectRef.current?.scroll({ top: 0 });
   };
 
   return (
@@ -86,22 +50,28 @@ export const WithdrawDialog2 = ({ setIsOpen }: DialogProps<DepositDialog2Props>)
           tw="w-[50%]"
           style={{ marginLeft: formState === 'form' ? 0 : '-50%', transition: 'margin 500ms' }}
         >
-          <DepositForm
+          <WithdrawForm
             amount={amount}
             setAmount={setAmount}
-            token={token}
-            onTokenSelect={() => setFormState('token-select')}
+            destinationAddress={destinationAddress}
+            setDestinationAddress={setDestinationAddress}
+            destinationChain={destinationChain}
+            onChainSelect={() => setFormState('chain-select')}
           />
         </div>
         <div
-          ref={tokenSelectRef}
+          ref={chainSelectRef}
           tw="w-[50%] overflow-scroll"
           style={{
             height: formState === 'form' ? 0 : '100%',
             maxHeight: isMobile ? '50vh' : '25rem',
           }}
         >
-          <TokenSelect token={token} setToken={setToken} onBack={onShowForm} />
+          <ChainSelect
+            selectedChain={destinationChain}
+            setSelectedChain={setDestinationChain}
+            onBack={onShowForm}
+          />
         </div>
       </div>
     </$Dialog>
