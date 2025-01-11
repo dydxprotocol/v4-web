b359423353e9b91fc31a90b445c7ce46ba183482
Tina Zheng <tina.s.zheng+github@gmail.com>
2025-01-10 21:46:47 -0500

    allow unsafe quotes

--- src/views/dialogs/DepositDialog2/queries.ts
+++ src/views/dialogs/DepositDialog2/queries.ts
@@ -120,6 +120,8 @@ async function getSkipRoutes(skipClient: SkipClient, token: DepositToken, amount
     destAssetChainID: DYDX_DEPOSIT_CHAIN,
     amountIn: parseUnits(amount, token.decimals).toString(),
     smartRelay: true,
+    // allow quotes even if they have large price impact, as the user would see the difference in fees anyway
+    allowUnsafe: true,
     smartSwapOptions: { evmSwaps: true },
   };
 
