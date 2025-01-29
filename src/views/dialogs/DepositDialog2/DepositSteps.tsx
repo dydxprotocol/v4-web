1753a54b911fb29b41a75a6e7d3c4a2dcd9c925f
Tina <59578595+tinaszheng@users.noreply.github.com>
2025-01-29 17:50:51 -0500

    feat: add deposit retry UI and error messages (#1475)

--- src/views/dialogs/DepositDialog2/DepositSteps.tsx
+++ src/views/dialogs/DepositDialog2/DepositSteps.tsx
@@ -1,5 +1,8 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
+import tw from 'twin.macro';
+
+import { ErrorExclamationIcon } from '@/icons';
 import ConnectingLine from '@/icons/connecting-line.svg';
 
 import { Icon, IconName } from '@/components/Icon';
@@ -10,7 +13,7 @@ import { DepositStep } from './utils';
 type DepositStepsProps = {
   steps: DepositStep[];
   currentStep: number;
-  showRetry: boolean;
+  currentStepError?: string;
   onRetry: () => void;
 };
 
@@ -18,25 +21,44 @@ const STEP_TYPE_TO_INFO: { [type: string]: { title: string; icon: ReactNode } } 
   network: {
     // TODO(deposit2.0): localization
     title: 'Switch networks',
-    icon: <Icon size="1.25rem" iconName={IconName.Switch} />,
+    icon: <Icon size="1.5rem" iconName={IconName.Switch} />,
   },
   approve: {
     title: 'Approve USDC',
-    icon: <Icon size="1.25rem" iconName={IconName.Usdc} />,
+    icon: <Icon size="1.5rem" iconName={IconName.Usdc} />,
   },
   deposit: {
     title: 'Confirm deposit',
-    icon: <Icon size="1.25rem" iconName={IconName.Deposit} />,
+    icon: <Icon size="1.5rem" iconName={IconName.Deposit} />,
   },
 };
 
-export const DepositSteps = ({ steps, currentStep, showRetry, onRetry }: DepositStepsProps) => {
+const SHAKE_DURATION = 1000;
+
+export const DepositSteps = ({
+  steps,
+  currentStep,
+  currentStepError,
+  onRetry,
+}: DepositStepsProps) => {
+  const [isShaking, setIsShaking] = useState(false);
+  useEffect(() => {
+    if (!currentStepError) {
+      setIsShaking(false);
+      return;
+    }
+
+    setIsShaking(true);
+    setTimeout(() => setIsShaking(false), SHAKE_DURATION);
+  }, [currentStepError]);
+
   return (
     <div tw="flex flex-col">
       {steps.map((step, i) => {
         const stepInfo = STEP_TYPE_TO_INFO[step.type]!;
+        const error = i === currentStep ? currentStepError : undefined;
         return (
-          <div key={step.type} tw="flex flex-col items-start">
+          <div key={step.type} tw="flex flex-col">
             <div tw="flex items-center gap-0.125">
               <div tw="relative p-0.5">
                 <LoadingSpinner
@@ -45,31 +67,43 @@ export const DepositSteps = ({ steps, currentStep, showRetry, onRetry }: Deposit
                   strokeWidth="2"
                   stroke=""
                   tw="absolute left-0 top-0 flex h-full w-full items-center justify-center text-color-accent"
-                  style={{ visibility: i === currentStep && !showRetry ? undefined : 'hidden' }}
+                  css={(i !== currentStep || currentStepError) && tw`invisible`}
                 />
-                <div tw="flex items-center justify-center rounded-4 bg-color-layer-5 p-0.5">
+                <div
+                  css={i === currentStep && tw`text-color-text-2`}
+                  tw="relative flex items-center justify-center rounded-4 bg-color-layer-5 p-0.375"
+                >
                   {stepInfo.icon}
+                  {error && (
+                    <ErrorExclamationIcon tw="absolute right-[-2px] top-[-2px] h-[12px] w-[12px] text-color-error" />
+                  )}
                 </div>
               </div>
-              <div tw="flex items-center gap-0.5">
-                <div
-                  tw="opacity-50 transition-all duration-300"
-                  style={{ opacity: i === currentStep ? '100%' : undefined }}
-                >
-                  {stepInfo.title}
+              <div tw="flex flex-1 items-center justify-between gap-0.5">
+                <div tw="flex flex-col gap-0.125">
+                  <div
+                    tw="transition-all duration-300"
+                    css={[i === currentStep ? tw`opacity-100` : tw`opacity-50`]}
+                  >
+                    {stepInfo.title}
+                  </div>
+                  {error && <div tw="text-small text-color-error">{error}</div>}
                 </div>
-                {showRetry && i === currentStep && (
+                {error && (
                   <button
-                    tw="rounded-0.5 border border-solid border-color-accent p-0.125 text-color-accent"
+                    css={isShaking && tw`animate-shake`}
+                    tw="flex items-center gap-0.375 rounded-0.5 border border-solid border-color-accent p-0.375 text-color-accent hover:bg-color-layer-4"
                     type="button"
                     onClick={onRetry}
                   >
-                    Retry
+                    <Icon iconName={IconName.Refresh} />
+                    {/* TODO(deposit2.0): localization */}
+                    <div>Retry</div>
                   </button>
                 )}
               </div>
             </div>
-            {i !== steps.length - 1 && <ConnectingLine tw="ml-[25px]" />}
+            {i !== steps.length - 1 && <ConnectingLine tw="ml-[25px] self-start" />}
           </div>
         );
       })}
