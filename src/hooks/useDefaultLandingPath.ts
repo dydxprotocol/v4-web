f2014540b3135f5c72a235f139e971e2fb42e954
jaredvu <jaredvu@gmail.com>
2025-01-30 15:22:03 -0800

    add error handling

--- src/hooks/useDefaultLandingPath.ts
+++ src/hooks/useDefaultLandingPath.ts
@@ -3,6 +3,8 @@ import { useMemo } from 'react';
 import { WEB_CONFIG_MAP } from '@/constants/networks';
 import { AppRoute, DEFAULT_TRADE_ROUTE } from '@/constants/routes';
 
+import { log } from '@/lib/telemetry';
+
 export interface WebDeploymentConfig {
   defaultPath: string | null;
 }
@@ -14,14 +16,23 @@ export const useDefaultLandingPath = (): string => {
 
     if (!defaultPath) return DEFAULT_TRADE_ROUTE;
 
-    if (defaultPath.startsWith('/')) {
-      defaultPath.split('/');
-
-      if (defaultPath.length > 1) {
-        if (Object.values(AppRoute).includes(defaultPath[0] as AppRoute)) {
-          return defaultPath;
-        }
+    try {
+      if (!defaultPath.startsWith('/')) {
+        throw new Error('Invalid default path');
       }
+
+      const splitPath = defaultPath.split('/');
+      if (splitPath.length < 1) {
+        throw new Error('Invalid default path');
+      }
+
+      if (!Object.values(AppRoute).includes(defaultPath[0] as AppRoute)) {
+        throw new Error('Invalid AppRoute');
+      }
+
+      return defaultPath;
+    } catch (e) {
+      log('useDefaultLandingPath', e, { defaultPath });
     }
 
     return DEFAULT_TRADE_ROUTE;
