490bed57b05aef95525003bede2f84953797f86a
Tina <59578595+tinaszheng@users.noreply.github.com>
2025-01-30 17:14:50 -0500

    fix: minor deposit flow fixes (#1487)

--- src/views/dialogs/DepositDialog2/DepositStatus.tsx
+++ src/views/dialogs/DepositDialog2/DepositStatus.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 
 import { BonsaiCore } from '@/bonsai/ontology';
 import { ArrowRightIcon } from '@radix-ui/react-icons';
@@ -39,13 +39,22 @@ export const DepositStatus = ({ txHash, chainId, onClose }: DepositStatusProps) 
     appQueryClient.invalidateQueries({ queryKey: ['validator', 'accountBalances'], exact: false });
   }, [deposit]);
 
-  if (!deposit) return null;
-
   // Use difference in free collateral value to determine that the subaccount sweep has finished
   const depositSuccess =
-    deposit.status === 'success' &&
+    deposit?.status === 'success' &&
     freeCollateral &&
     !initialFreeCollateral.current?.eq(freeCollateral);
+
+  const statusDescription = useMemo(() => {
+    if (depositSuccess) return 'Your funds are now available for trading.';
+
+    if (deposit?.isInstantDeposit)
+      return 'Your funds will be available soon, and you may safely close this window.';
+
+    return 'You may safely close this window.';
+  }, [deposit, depositSuccess]);
+
+  if (!deposit) return null;
 
   return (
     <div tw="flex flex-col gap-1 px-2 pb-1.5 pt-2.5">
@@ -57,11 +66,7 @@ export const DepositStatus = ({ txHash, chainId, onClose }: DepositStatusProps) 
         )}
         <div tw="flex flex-col items-center gap-0.375 px-3 py-1 text-center">
           <div tw="text-large">{!depositSuccess ? 'Deposit in progress' : 'Deposit complete'}</div>
-          <div tw="text-color-text-0">
-            {!depositSuccess
-              ? 'Your funds will be available soon, and you can safely close this window.'
-              : 'Your funds are now available for trading.'}
-          </div>
+          <div tw="text-color-text-0">{statusDescription}</div>
         </div>
       </div>
       <div tw="flex items-center justify-between self-stretch">
