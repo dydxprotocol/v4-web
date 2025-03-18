2a6bcfc028077311344abdff20f46decc62a7b7c
jaredvu <jaredvu@gmail.com>
2025-03-18 13:59:50 -0700

    beef up logs and fix bugs

--- src/bonsai/lifecycles/usdcRebalanceLifecycle.ts
+++ src/bonsai/lifecycles/usdcRebalanceLifecycle.ts
@@ -3,6 +3,7 @@ import BigNumber from 'bignumber.js';
 
 import { AMOUNT_RESERVED_FOR_GAS_USDC, AMOUNT_USDC_BEFORE_REBALANCE } from '@/constants/account';
 import { TransactionMemo } from '@/constants/analytics';
+import { timeUnits } from '@/constants/time';
 import { USDC_DECIMALS } from '@/constants/tokens';
 import { WalletNetworkType } from '@/constants/wallets';
 
@@ -15,11 +16,14 @@ import { getLocalWalletNonce, getSourceAccount } from '@/state/walletSelectors';
 import { isBlockedGeo } from '@/lib/compliance';
 import { localWalletManager } from '@/lib/hdKeyManager';
 import { MaybeBigNumber, MustBigNumber } from '@/lib/numbers';
+import { objectEntries } from '@/lib/objectHelpers';
+import { sleep } from '@/lib/timeUtils';
 
 import { createSemaphore, SupersededError } from '../lib/semaphore';
 import { logBonsaiError, logBonsaiInfo } from '../logs';
 import { BonsaiCore } from '../ontology';
 import { createValidatorStoreEffect } from '../rest/lib/indexerQueryStoreEffect';
+import { selectChildSubaccountSummaries } from '../selectors/account';
 import { selectParentSubaccountInfo } from '../socketSelectors';
 import { ComplianceStatus } from '../types/summaryTypes';
 
@@ -52,18 +56,16 @@ const selectTxAuthorizedAccount = createAppSelector(
 
     if (!canWalletTransact) return undefined;
 
-    const subaccountClient = new SubaccountClient(
-      localDydxWallet!,
-      parentSubaccountInfo.subaccount
-    );
-
     return {
-      subaccountClient,
+      localDydxWallet,
       sourceAccount,
       parentSubaccountInfo,
     };
   }
 );
+
+// Sleep time between rebalances to ensure that the subaccount has time to process the previous transaction
+const SLEEP_TIME = timeUnits.second * 10;
 
 /**
  * @description Lifecycle for rebalancing USDC across chains. This will handle auto-deposits from dYdX Wallet as well as auto-withdrawals to dYdX Wallet.
@@ -73,19 +75,22 @@ export function setUpUsdcRebalanceLifecycle(store: RootStore) {
     [
       selectTxAuthorizedAccount,
       BonsaiCore.account.balances.data,
+      selectChildSubaccountSummaries,
       selectHasNonExpiredPendingWithdraws,
     ],
-    (txAuthorizedAccount, balances, hasNonExpiredPendingWithdraws) => {
-      if (!txAuthorizedAccount) {
+    (txAuthorizedAccount, balances, childSubaccountSummaries, hasNonExpiredPendingWithdraws) => {
+      if (!txAuthorizedAccount || childSubaccountSummaries == null) {
         return undefined;
       }
 
-      const { subaccountClient, sourceAccount } = txAuthorizedAccount;
+      const { localDydxWallet, sourceAccount, parentSubaccountInfo } = txAuthorizedAccount;
 
       return {
-        subaccountClient,
+        localDydxWallet,
+        parentSubaccountInfo,
         sourceAccount,
         balances,
+        childSubaccountSummaries,
         hasNonExpiredPendingWithdraws,
       };
     }
@@ -101,7 +106,13 @@ export function setUpUsdcRebalanceLifecycle(store: RootStore) {
       }
 
       async function rebalanceWalletFunds() {
-        const { subaccountClient, balances, hasNonExpiredPendingWithdraws } = data!;
+        const {
+          localDydxWallet,
+          balances,
+          childSubaccountSummaries,
+          hasNonExpiredPendingWithdraws,
+          parentSubaccountInfo,
+        } = data!;
         const usdcBalance = balances.usdcAmount;
         const usdcBalanceBN: BigNumber | undefined = MaybeBigNumber(usdcBalance);
 
@@ -115,23 +126,53 @@ export function setUpUsdcRebalanceLifecycle(store: RootStore) {
               .toFixed(USDC_DECIMALS);
 
             logBonsaiInfo('usdcRebalanceLifecycle', 'depositing excess USDC into subaccount 0', {
+              balance: usdcBalance,
               amountToDeposit,
               targetAmount: AMOUNT_RESERVED_FOR_GAS_USDC,
             });
+
+            const subaccountClient = new SubaccountClient(
+              localDydxWallet!,
+              parentSubaccountInfo.subaccount
+            );
 
             await compositeClient.depositToSubaccount(
               subaccountClient,
               amountToDeposit,
               TransactionMemo.depositToSubaccount
             );
+
+            await sleep(SLEEP_TIME);
           } else if (shouldWithdraw) {
             const amountToWithdraw = MustBigNumber(AMOUNT_RESERVED_FOR_GAS_USDC)
               .minus(usdcBalanceBN)
               .toFixed(USDC_DECIMALS);
 
+            const maybeSubaccountNumber = objectEntries(childSubaccountSummaries).find(
+              ([_, summary]) => {
+                if (
+                  summary.freeCollateral.gt(amountToWithdraw) &&
+                  (summary.marginUsage == null || summary.marginUsage.lt(0.4))
+                ) {
+                  return true;
+                }
+
+                return false;
+              }
+            )?.[0];
+
+            if (maybeSubaccountNumber == null) {
+              return;
+            }
+
+            const subaccountNumber = Number(maybeSubaccountNumber);
+            const subaccountClient = new SubaccountClient(localDydxWallet!, subaccountNumber);
+
             logBonsaiInfo('usdcRebalanceLifecycle', 'withdrawing funds for gas', {
+              balance: usdcBalance,
               amountToWithdraw,
               targetAmount: AMOUNT_RESERVED_FOR_GAS_USDC,
+              subaccountNumber,
             });
 
             await compositeClient.withdrawFromSubaccount(
@@ -140,6 +181,8 @@ export function setUpUsdcRebalanceLifecycle(store: RootStore) {
               undefined,
               TransactionMemo.withdrawFromSubaccount
             );
+
+            await sleep(SLEEP_TIME);
           }
         }
       }
