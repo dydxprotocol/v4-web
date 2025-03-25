2753b8582ce7125acdaf824954c1d6c25742fe28
jaredvu <jaredvu@gmail.com>
2025-03-25 15:13:47 -0700

    update invalid address symbol

--- src/views/dialogs/TransferDialogs/WithdrawDialog2/AddressInput.tsx
+++ src/views/dialogs/TransferDialogs/WithdrawDialog2/AddressInput.tsx
@@ -34,7 +34,7 @@ export const AddressInput = ({
 }: AddressInputProps) => {
   const stringGetter = useStringGetter();
   const { sourceAccount } = useAccounts();
-  const [invalidAddress, setInvalidAddress] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
 
   const onValueChange: EventHandler<SyntheticInputEvent> = (e) => {
     onChange(e.target.value);
@@ -43,13 +43,11 @@ export const AddressInput = ({
   const hasValidAddress = isValidWithdrawalAddress(value, destinationChain);
 
   const onBlur = () => {
-    if (value.length > 0 && !hasValidAddress) {
-      setInvalidAddress(true);
-    }
+    setIsFocused(false);
   };
 
   const onFocus = () => {
-    setInvalidAddress(false);
+    setIsFocused(true);
   };
 
   return (
@@ -57,7 +55,7 @@ export const AddressInput = ({
       <div tw="flex flex-1 flex-col gap-0.5 text-small">
         <div>
           {stringGetter({ key: STRING_KEYS.ADDRESS })}{' '}
-          {invalidAddress && (
+          {!hasValidAddress && !isFocused && value.trim() !== '' && (
             <WithTooltip tooltipString={stringGetter({ key: STRING_KEYS.INVALID_ADDRESS_TITLE })}>
               <Icon tw="text-color-error" size="0.75rem" iconName={IconName.Warning} />
             </WithTooltip>
