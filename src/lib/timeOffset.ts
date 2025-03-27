9e5eeedb039861a718705748314a5d5d4c87408a
tyleroooo <tyler@dydx.exchange>
2025-03-27 17:29:13 -0400

    fix: move floor (#1598)

--- src/lib/timeOffset.ts
+++ src/lib/timeOffset.ts
@@ -32,7 +32,7 @@ async function getTimestampOffset() {
       if (!isFinite(serverMs)) {
         return 0;
       }
-      return Math.floor(utils.calculateClockOffsetFromFetchDateHeader(start, serverMs, end));
+      return utils.calculateClockOffsetFromFetchDateHeader(start, serverMs, end);
     } catch (e) {
       return 0;
     }
