fbda302251080d3798763531d35116d19c8a5e8f
Tyler <tyler@dydx.exchange>
2025-04-24 16:35:23 -0400

    fix

--- src/state/notificationsSelectors.ts
+++ src/state/notificationsSelectors.ts
@@ -1,5 +1,3 @@
 import { type RootState } from './_store';
 
-export const getAbacusNotifications = (state: RootState) => state.notifications.abacusNotifications;
-
 export const getCustomNotifications = (state: RootState) => state.notifications.customNotifications;
