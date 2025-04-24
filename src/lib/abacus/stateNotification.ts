7f9d0d77ae9559035bafe369ce775f55c5f9687a
Tyler <tyler@dydx.exchange>
2025-04-24 11:00:07 -0400

    fix

--- src/lib/abacus/stateNotification.ts
+++ src/lib/abacus/stateNotification.ts
@@ -1,19 +1,10 @@
 // eslint-disable-next-line max-classes-per-file
 import { kollections } from '@dydxprotocol/v4-abacus';
 
-import type {
-  AbacusNotification,
-  AbacusStateNotificationProtocol,
-  PerpetualState,
-  PerpetualStateChanges,
-} from '@/constants/abacus';
-import { Changes } from '@/constants/abacus';
+import type { AbacusNotification, AbacusStateNotificationProtocol } from '@/constants/abacus';
 
 import { type RootStore } from '@/state/_store';
-import { setInputs } from '@/state/inputs';
 import { updateNotifications } from '@/state/notifications';
-
-import { Nullable } from '@/lib/typeUtils';
 
 class AbacusStateNotifier implements AbacusStateNotificationProtocol {
   private store: RootStore | undefined;
@@ -24,20 +15,7 @@ class AbacusStateNotifier implements AbacusStateNotificationProtocol {
     this.store?.dispatch(updateNotifications(notifications.toArray()));
   }
 
-  stateChanged(
-    updatedState: Nullable<PerpetualState>,
-    incomingChanges: Nullable<PerpetualStateChanges>
-  ): void {
-    if (!this.store) return;
-    const { dispatch } = this.store;
-    const changes = new Set(incomingChanges?.changes.toArray() ?? []);
-
-    if (updatedState) {
-      if (changes.has(Changes.input)) {
-        dispatch(setInputs(updatedState.input));
-      }
-    }
-  }
+  stateChanged() {}
 
   lastOrderChanged() {}
 
