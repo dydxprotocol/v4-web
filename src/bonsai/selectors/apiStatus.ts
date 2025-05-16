55a3ccf2bc604ac30b11a9cafa2ff5c5df65e02d
Tyler <tyler@dydx.exchange>
2025-05-16 11:37:18 -0400

    even-more-bias

--- src/bonsai/selectors/apiStatus.ts
+++ src/bonsai/selectors/apiStatus.ts
@@ -26,7 +26,7 @@ export const selectLatestValidatorHeight = createAppSelector(
 
 // not a selector since we use current time
 // we return a very conservative estimate, aiming to return the smallest reasonable block height estimate
-export const estimateLiveValidatorHeight = (state: RootState) => {
+export const estimateLiveValidatorHeight = (state: RootState, biasMultiplier: number = 1.0) => {
   const latestEntry = getLatestHeightEntry(selectRawValidatorHeightData(state));
   const heightAtRequest = latestEntry?.response?.height;
   if (latestEntry == null || heightAtRequest == null) {
@@ -37,7 +37,6 @@ export const estimateLiveValidatorHeight = (state: RootState) => {
   const now = new Date().getTime();
   const elapsedTime = now - responseTime;
 
-  // multiply by .9 to be extra conservative
-  const elapsedBlocksEst = Math.floor(elapsedTime / (ESTIMATED_BLOCK_TIME * 0.9));
+  const elapsedBlocksEst = Math.floor(elapsedTime / (ESTIMATED_BLOCK_TIME * biasMultiplier));
   return heightAtRequest + elapsedBlocksEst;
 };
