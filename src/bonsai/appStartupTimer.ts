93bbaa25234153659b99549431e13fb5ced04920
Tyler <tyler@dydx.exchange>
2025-06-04 18:14:42 -0400

    fix

---
+++ src/bonsai/appStartupTimer.ts
@@ -0,0 +1,41 @@
+import { timeUnits } from '@/constants/time';
+
+import { logBonsaiInfo, REQUEST_TIME_SAMPLE_RATE } from './logs';
+
+type StartupStats = {
+  start: number | undefined;
+  renderApp?: number | undefined;
+  renderTrade?: number | undefined;
+  startMarkets?: number | undefined;
+  loadedMarkets?: number | undefined;
+  setCurrentMarket?: number | undefined;
+  startOrderbook?: number | undefined;
+  loadedOrderbook?: number | undefined;
+  startCandles?: number | undefined;
+  loadedCandles?: number | undefined;
+};
+const stats: StartupStats = {
+  start: performance.now(),
+};
+
+const TOO_LONG_STARTUP = 15 * timeUnits.second;
+
+const firstSeenPath = document.location.pathname;
+
+export const AppStartupTimer = {
+  timeIfFirst(t: keyof StartupStats) {
+    if (stats[t] == null) {
+      stats[t] = performance.now();
+      if (
+        (t === 'loadedCandles' || t === 'loadedOrderbook') &&
+        stats.loadedCandles != null &&
+        stats.loadedOrderbook != null &&
+        performance.now() < TOO_LONG_STARTUP &&
+        firstSeenPath.indexOf('/trade') >= 0 &&
+        Math.random() < REQUEST_TIME_SAMPLE_RATE
+      ) {
+        logBonsaiInfo('AppStartupTimer', 'App started', stats);
+      }
+    }
+  },
+};
