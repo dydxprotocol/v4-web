dfd4263981802658a4eb30b4a59351fd4614b789
jaredvu <jaredvu@gmail.com>
2025-06-09 12:32:39 -0700

    add trending icon

--- src/pages/markets/simple-ui/markets-view/PositionRow.tsx
+++ src/pages/markets/simple-ui/markets-view/PositionRow.tsx
@@ -9,6 +9,7 @@ import { useAppSelectorWithArgs } from '@/hooks/useParameterizedSelector';
 import { useStringGetter } from '@/hooks/useStringGetter';
 
 import { AssetIcon } from '@/components/AssetIcon';
+import { Icon, IconName } from '@/components/Icon';
 import { Output, OutputType } from '@/components/Output';
 
 import { getIndexerPositionSideStringKey } from '@/lib/enumToStringKeyHelpers';
@@ -46,7 +47,10 @@ export const PositionRow = ({
       to={`${AppRoute.Trade}/${market.ticker}`}
     >
       <div tw="row min-w-0 flex-grow-0 gap-0.5">
-        <AssetIcon logoUrl={market.logo} tw="size-[2.75rem] min-w-[2.75rem]" />
+        <div tw="row relative size-[2.75rem] min-w-[2.75rem] justify-center rounded-[50%]">
+          <AssetIcon logoUrl={market.logo} tw="size-[2.75rem] min-w-[2.75rem]" />
+          <TrendIcon positionSide={position.side} tw="absolute bottom-[-3px] right-[-4px]" />
+        </div>
         <div tw="flexColumn gap-0.25">
           <span tw="whitespace-nowrap leading-[1rem]">
             <span tw="mr-0.25" css={{ color: sideColor }}>
@@ -80,5 +84,35 @@ export const PositionRow = ({
         />
       </div>
     </Link>
+  );
+};
+
+const TrendIcon = ({
+  className,
+  positionSide,
+}: {
+  className?: string;
+  positionSide: IndexerPositionSide;
+}) => {
+  return (
+    <div
+      className={className}
+      tw="row size-1 min-w-1 justify-center rounded-[50%] border-2 border-solid border-color-layer-2 text-color-layer-2"
+      css={{
+        backgroundColor:
+          positionSide === IndexerPositionSide.LONG
+            ? 'var(--color-positive)'
+            : 'var(--color-negative)',
+      }}
+    >
+      <Icon
+        iconName={
+          positionSide === IndexerPositionSide.LONG ? IconName.TrendingUp : IconName.TrendingDown
+        }
+        css={{
+          '--icon-size': '0.6rem',
+        }}
+      />
+    </div>
   );
 };
