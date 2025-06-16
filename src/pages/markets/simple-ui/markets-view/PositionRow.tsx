6d903ca26a61800c935bd8d7c1a1e7852e3ad3e7
Jared Vu <jaredvu@gmail.com>
2025-06-16 09:39:42 -0700

    feat(simple-ui): Position Row updates (#1761)

--- src/pages/markets/simple-ui/markets-view/PositionRow.tsx
+++ src/pages/markets/simple-ui/markets-view/PositionRow.tsx
@@ -2,6 +2,7 @@ import { BonsaiHelpers } from '@/bonsai/ontology';
 import { SubaccountPosition } from '@/bonsai/types/summaryTypes';
 import { Link } from 'react-router-dom';
 
+import { PositionSortType } from '@/constants/marketList';
 import { AppRoute } from '@/constants/routes';
 import { IndexerPositionSide } from '@/types/indexer/indexerApiGen';
 
@@ -10,9 +11,18 @@ import { useStringGetter } from '@/hooks/useStringGetter';
 
 import { AssetIcon } from '@/components/AssetIcon';
 import { Icon, IconName } from '@/components/Icon';
-import { Output, OutputType } from '@/components/Output';
+import { MarginUsageRing } from '@/components/MarginUsageRing';
+import { Output, OutputType, ShowSign } from '@/components/Output';
+import { Tag } from '@/components/Tag';
 
+import { getSubaccountEquity } from '@/state/accountSelectors';
+import { useAppSelector } from '@/state/appTypes';
+import { getSimpleUISortPositionsBy } from '@/state/appUiConfigsSelectors';
+
+import { assertNever } from '@/lib/assertNever';
+import { calc } from '@/lib/do';
 import { getIndexerPositionSideStringKey } from '@/lib/enumToStringKeyHelpers';
+import { MustBigNumber } from '@/lib/numbers';
 
 export const PositionRow = ({
   className,
@@ -22,6 +32,8 @@ export const PositionRow = ({
   position: SubaccountPosition;
 }) => {
   const stringGetter = useStringGetter();
+  const subaccountEquity = useAppSelector(getSubaccountEquity);
+  const positionSortType = useAppSelector(getSimpleUISortPositionsBy);
   const market = useAppSelectorWithArgs(
     BonsaiHelpers.markets.selectMarketSummaryById,
     position.market
@@ -29,16 +41,130 @@ export const PositionRow = ({
 
   if (!market) return null;
 
-  const percentChangeColor = market.percentChange24h
-    ? market.percentChange24h >= 0
-      ? 'var(--color-positive)'
-      : 'var(--color-negative)'
-    : 'var(--color-text-1)';
+  const pnlColor = position.updatedUnrealizedPnl.gt(0)
+    ? 'var(--color-positive)'
+    : position.updatedUnrealizedPnl.lt(0)
+      ? 'var(--color-negative)'
+      : 'var(--color-text-1)';
 
   const sideString = stringGetter({ key: getIndexerPositionSideStringKey(position.side) });
 
   const sideColor =
     position.side === IndexerPositionSide.LONG ? 'var(--color-positive)' : 'var(--color-negative)';
+
+  const shouldCompact =
+    market.stepSizeDecimals >= 0 && MustBigNumber(position.signedSize).abs().gte(100_000);
+
+  const slotRight = calc(() => {
+    switch (positionSortType) {
+      case PositionSortType.Leverage: {
+        const marginUsage =
+          subaccountEquity != null && subaccountEquity !== 0
+            ? position.marginValueInitial.div(subaccountEquity).toNumber()
+            : undefined;
+
+        return (
+          <div tw="flex flex-col items-end gap-0.25 text-end">
+            <Output
+              tw="text-color-text-2"
+              withSubscript
+              type={OutputType.Fiat}
+              value={position.marginValueInitial}
+            />
+            <span tw="row gap-0.25 text-color-text-1 font-small-book">
+              {marginUsage && (
+                <Output
+                  showSign={ShowSign.Both}
+                  type={OutputType.Percent}
+                  value={marginUsage}
+                  slotRight={<MarginUsageRing tw="ml-0.25" value={marginUsage} />}
+                />
+              )}
+            </span>
+          </div>
+        );
+      }
+      case PositionSortType.Price: {
+        const priceChangeBN = MustBigNumber(market.priceChange24H);
+        const priceChangeColor = priceChangeBN.gt(0)
+          ? 'var(--color-positive)'
+          : priceChangeBN.lt(0)
+            ? 'var(--color-negative)'
+            : 'var(--color-text-1)';
+
+        return (
+          <div tw="flex flex-col items-end gap-0.25 text-end">
+            <Output
+              tw="text-color-text-2"
+              withSubscript
+              type={OutputType.Fiat}
+              value={market.oraclePrice}
+              fractionDigits={market.tickSizeDecimals}
+            />
+            <span
+              tw="row gap-0.25 text-color-text-1 font-small-book"
+              css={{
+                color: priceChangeColor,
+              }}
+            >
+              <Output
+                showSign={ShowSign.Both}
+                type={OutputType.Fiat}
+                value={market.priceChange24H}
+              />
+              {market.percentChange24h != null && (
+                <Output
+                  type={OutputType.Percent}
+                  value={market.percentChange24h}
+                  slotLeft="("
+                  slotRight=")"
+                />
+              )}
+            </span>
+          </div>
+        );
+      }
+
+      case PositionSortType.Pnl:
+      case PositionSortType.Notional:
+        return (
+          <div tw="flex flex-col items-end gap-0.25 text-end">
+            <Output
+              tw="text-color-text-2"
+              withSubscript
+              type={OutputType.Fiat}
+              value={position.notional}
+            />
+            <span
+              tw="row gap-0.25 text-color-text-1 font-small-book"
+              css={{
+                color: pnlColor,
+              }}
+            >
+              <Output
+                showSign={ShowSign.Both}
+                type={OutputType.Fiat}
+                value={position.updatedUnrealizedPnl}
+              />
+              {position.updatedUnrealizedPnlPercent && (
+                <Output
+                  type={OutputType.Percent}
+                  value={position.updatedUnrealizedPnlPercent}
+                  slotLeft="("
+                  slotRight=")"
+                />
+              )}
+            </span>
+          </div>
+        );
+
+      default: {
+        assertNever(positionSortType);
+      }
+    }
+
+    return null;
+  });
 
   return (
     <Link
@@ -52,37 +178,29 @@ export const PositionRow = ({
           <TrendIcon positionSide={position.side} tw="absolute bottom-[-3px] right-[-4px]" />
         </div>
         <div tw="flexColumn gap-0.25">
-          <span tw="whitespace-nowrap leading-[1rem]">
-            <span tw="mr-0.25" css={{ color: sideColor }}>
-              {sideString}
-            </span>
+          <span tw="row gap-0.25 whitespace-nowrap leading-[1rem]">
             {market.displayableAsset}
+            <Tag tw="bg-color-layer-4">
+              <Output
+                type={OutputType.Multiple}
+                value={position.leverage}
+                tw="text-color-text-1"
+                fractionDigits={0}
+              />
+            </Tag>
           </span>
-          <Output
-            tw="text-color-text-1 font-mini-book"
-            type={OutputType.Fiat}
-            value={position.notional}
-          />
+          <span tw="row gap-0.25 font-small-book">
+            <span css={{ color: sideColor }}>{sideString}</span>
+            <Output
+              tw="text-color-text-0"
+              type={shouldCompact ? OutputType.CompactNumber : OutputType.Number}
+              value={position.signedSize}
+            />
+          </span>
         </div>
       </div>
 
-      <div tw="flex flex-col items-end gap-0.25 text-end">
-        <Output
-          tw="text-color-text-2"
-          withSubscript
-          type={OutputType.Fiat}
-          value={market.oraclePrice}
-          fractionDigits={market.tickSizeDecimals}
-        />
-        <Output
-          tw="font-mini-book"
-          css={{
-            color: percentChangeColor,
-          }}
-          type={OutputType.Percent}
-          value={market.percentChange24h}
-        />
-      </div>
+      {slotRight}
     </Link>
   );
 };
