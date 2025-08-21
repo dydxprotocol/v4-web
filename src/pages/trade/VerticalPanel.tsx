2d845bc410f9d4951dcd67ae03629060376050de
Mumtahin Farabi <mfarabi619@gmail.com>
2025-08-21 13:55:08 -0600

    Revert "remove orderbook from vertical pannel, trades only baby"
    This reverts commit c841ebd85fb5b0f95bc2205d60555878d43be6ff.

--- src/pages/trade/VerticalPanel.tsx
+++ src/pages/trade/VerticalPanel.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import styled from 'styled-components';
 
 import { TradeLayouts } from '@/constants/layout';
 import { STRING_KEYS } from '@/constants/localization';
+import { ORDERBOOK_HEADER_HEIGHT, ORDERBOOK_ROW_HEIGHT } from '@/constants/orderbook';
 
 import { useStringGetter } from '@/hooks/useStringGetter';
 
 import { Tabs } from '@/components/Tabs';
+import { CanvasOrderbook } from '@/views/CanvasOrderbook/CanvasOrderbook';
 import { LiveTrades } from '@/views/tables/LiveTrades';
 
 enum Tab {
+  Orderbook = 'Orderbook',
   Trades = 'Trades',
-  // TODO: Add Liquidity Display
 }
 
 const HISTOGRAM_SIDES_BY_LAYOUT = {
@@ -22,43 +24,43 @@ const HISTOGRAM_SIDES_BY_LAYOUT = {
 
 export const VerticalPanel = ({ tradeLayout }: { tradeLayout: TradeLayouts }) => {
   const stringGetter = useStringGetter();
-  const [value, setValue] = useState(Tab.Trades);
-  // const [rowsPerSide, setRowsPerSide] = useState<number | undefined>(undefined);
+  const [value, setValue] = useState(Tab.Orderbook);
+  const [rowsPerSide, setRowsPerSide] = useState<number | undefined>(undefined);
 
-  // const canvasOrderbookRef = useRef<HTMLDivElement>(null);
-  // const canvasOrderbook = canvasOrderbookRef.current;
+  const canvasOrderbookRef = useRef<HTMLDivElement>(null);
+  const canvasOrderbook = canvasOrderbookRef.current;
 
-  // const calculateNumRows = useCallback((orderbookHeight: number) => {
-  //   const maxNumRowsToRender = Math.floor(
-  //     (orderbookHeight - ORDERBOOK_HEADER_HEIGHT - ORDERBOOK_ROW_HEIGHT) /
-  //       (2 * ORDERBOOK_ROW_HEIGHT)
-  //   );
-  //   setRowsPerSide(maxNumRowsToRender);
-  // }, []);
+  const calculateNumRows = useCallback((orderbookHeight: number) => {
+    const maxNumRowsToRender = Math.floor(
+      (orderbookHeight - ORDERBOOK_HEADER_HEIGHT - ORDERBOOK_ROW_HEIGHT) /
+        (2 * ORDERBOOK_ROW_HEIGHT)
+    );
+    setRowsPerSide(maxNumRowsToRender);
+  }, []);
 
-  // useEffect(() => {
-  //   const resizeObserver = new ResizeObserver((entries) => {
-  //     entries.forEach((entry) => {
-  //       if (entry.contentBoxSize[0]) {
-  //         calculateNumRows(entry.contentBoxSize[0].blockSize);
-  //       } else {
-  //         calculateNumRows(entry.contentRect.height);
-  //       }
-  //     });
-  //   });
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.contentBoxSize[0]) {
+          calculateNumRows(entry.contentBoxSize[0].blockSize);
+        } else {
+          calculateNumRows(entry.contentRect.height);
+        }
+      });
+    });
 
-  //   // if (canvasOrderbook) {
-  //   //   resizeObserver.observe(canvasOrderbook);
-  //   // }
+    if (canvasOrderbook) {
+      resizeObserver.observe(canvasOrderbook);
+    }
 
-  //   return () => {
-  //     // if (canvasOrderbook) {
-  //     //   resizeObserver.unobserve(canvasOrderbook);
-  //     // } else {
-  //     resizeObserver.disconnect();
-  //     // }
-  //   };
-  // }, [calculateNumRows]);
+    return () => {
+      if (canvasOrderbook) {
+        resizeObserver.unobserve(canvasOrderbook);
+      } else {
+        resizeObserver.disconnect();
+      }
+    };
+  }, [calculateNumRows, canvasOrderbook]);
 
   return (
     <$Tabs
@@ -69,19 +71,19 @@ export const VerticalPanel = ({ tradeLayout }: { tradeLayout: TradeLayouts }) =>
         setValue(v);
       }}
       items={[
-        // {
-        //   asChild: true,
-        //   content: (
-        //     <CanvasOrderbook
-        //       histogramSide={HISTOGRAM_SIDES_BY_LAYOUT[tradeLayout]}
-        //       rowsPerSide={rowsPerSide}
-        //     />
-        //   ),
-        //   label: stringGetter({ key: STRING_KEYS.ORDERBOOK_SHORT }),
-        //   value: Tab.Orderbook,
-        //   forceMount: true,
-        //   ref: canvasOrderbookRef,
-        // },
+        {
+          asChild: true,
+          content: (
+            <CanvasOrderbook
+              histogramSide={HISTOGRAM_SIDES_BY_LAYOUT[tradeLayout]}
+              rowsPerSide={rowsPerSide}
+            />
+          ),
+          label: stringGetter({ key: STRING_KEYS.ORDERBOOK_SHORT }),
+          value: Tab.Orderbook,
+          forceMount: true,
+          ref: canvasOrderbookRef,
+        },
         {
           content: <LiveTrades histogramSide={HISTOGRAM_SIDES_BY_LAYOUT[tradeLayout]} />,
           label: stringGetter({ key: STRING_KEYS.TRADES }),
