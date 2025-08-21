ee57769ff546e50560e44d3db0e245957ef0b223
Tyler <tyler@dydx.exchange>
2025-08-21 14:23:50 -0400

    v0

---
+++ src/bonsai/rest/permissionedKeys.ts
@@ -0,0 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { timeUnits } from '@/constants/time';
+import { AccountAuthenticator } from '@/constants/validators';
+
+import { getUserWalletAddress } from '@/state/accountInfoSelectors';
+import { useAppSelector } from '@/state/appTypes';
+
+import { parseToPrimitives } from '@/lib/abacus/parseToPrimitives';
+import { isPresent } from '@/lib/typeUtils';
+
+import { useCompositeClient } from './lib/useIndexer';
+
+export function useAuthorizedAccounts() {
+  const address = useAppSelector(getUserWalletAddress);
+  const client = useCompositeClient();
+
+  return useQuery({
+    queryKey: ['validator', 'permissionedKeys', 'authorizedAccounts', address, client.key],
+    enabled: isPresent(address) && isPresent(client),
+    queryFn: async (): Promise<AccountAuthenticator[]> => {
+      if (!address || !client.compositeClient) {
+        throw new Error('Invalid authorized accounts query state');
+      }
+
+      const response = await client.compositeClient.getAuthenticators(address);
+      const authenticators = parseToPrimitives(response);
+      return authenticators.accountAuthenticators;
+    },
+    staleTime: timeUnits.hour,
+    refetchInterval: timeUnits.hour,
+  });
+}
