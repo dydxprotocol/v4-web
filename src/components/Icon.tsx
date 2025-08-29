503060edc73d2a89bc27c86e0bfa0811dcf78d75
qardpeet <qard.peet@gmail.com>
2025-08-29 18:44:15 +0400

    wip

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -109,6 +109,7 @@ import {
   ShowIcon,
   SocialLoginIcon,
   SocialXIcon,
+  SolIcon,
   SparklesIcon,
   SpeechBubbleIcon,
   StarIcon,
@@ -292,6 +293,7 @@ export enum IconName {
   Withdraw = 'Withdraw',
   XCircle = 'XCircle',
   SocialX = 'SocialX',
+  Sol = 'Sol',
 }
 
 const icons = {
@@ -436,6 +438,7 @@ const icons = {
   [IconName.Withdraw]: WithdrawIcon,
   [IconName.XCircle]: XCircleIcon,
   [IconName.SocialX]: SocialXIcon,
+  [IconName.Sol]: SolIcon,
 } as Record<IconName, ElementType | undefined>;
 
 // we load reward-start async because it's gigantic for some reason
