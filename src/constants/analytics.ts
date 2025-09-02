f2dab99c8eee6af49aa0e72c929a8093b20c7898
Paata Kardava <patplaysg@gmail.com>
2025-09-02 22:39:08 +0400

    feat: custom affiliate links (#1819)

--- src/constants/analytics.ts
+++ src/constants/analytics.ts
@@ -412,6 +412,7 @@ export const AnalyticsEvents = unionize(
     }>(),
     AffiliateSaveReferralAddress: ofType<{ affiliateAddress: string }>(),
     AffiliateURLCopied: ofType<{ url: string }>(),
+    AffiliateReferralCodeUpdated: ofType<{ newCode: string }>(),
 
     // Favoriting Markets
     FavoriteMarket: ofType<{ marketId: string }>(),
