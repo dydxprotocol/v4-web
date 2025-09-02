3a3940ca75af45c66afc7ee09a521a2c400b136a
Fionn Lennon <fionn@legencymedia.com>
2025-09-02 10:52:34 +0100

    Add UTM parameters to rebate banner URL for tracking
    Signed-off-by: Fionn Lennon <fionn@legencymedia.com>

--- src/pages/markets/MarketsBanners.tsx
+++ src/pages/markets/MarketsBanners.tsx
@@ -121,7 +121,7 @@ export const MarketsBanners = ({
           <Button
             action={ButtonAction.Primary}
             type={ButtonType.Link}
-            href="https://dydx.trade/DYDX"
+            href="https://dydx.trade/DYDX?utm_source=markets&utm_medium=markets-banner&utm_campaign=02092025-markets-surge-banner-dydx&utm_term=&utm_content=surge-banner"
             tw="relative z-10 w-12"
           >
             {stringGetter({ key: STRING_KEYS.REBATE_BANNER_CTA })}
