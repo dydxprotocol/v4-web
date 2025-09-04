7310ef68bc4bae2c490ce0823da6eaf923c5c4e2
D3Portillo <d3portillo@gmail.com>
2025-09-05 01:01:17 +0200

    chores: remove solana

--- src/constants/solana.ts
+++
@@ -1,7 +0,0 @@
-import { isMainnet } from './networks';
-
-export const getSolanaChainId = () => {
-  return isMainnet ? SOLANA_MAINNET_ID : 'solana-devnet';
-};
-
-export const SOLANA_MAINNET_ID = 'solana';
