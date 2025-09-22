1e42a635d100111733ad9fd92bcda48e286ada3e
Tina Zheng <tina.s.zheng+github@gmail.com>
2025-09-21 21:05:03 -0400

    finish swap functionality

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,11 +1,6 @@
 import environments from '../../public/configs/v1/env.json';
 
-export const CURRENT_MODE = ({
-  production: 'MAINNET',
-  testnet: 'TESTNET',
-  staging: 'DEV',
-  development: 'DEV',
-}[import.meta.env.MODE] ?? 'MAINNET') as 'MAINNET' | 'TESTNET' | 'DEV';
+export const CURRENT_MODE = 'MAINNET';
 
 export const isMainnet = CURRENT_MODE === 'MAINNET';
 export const isTestnet = CURRENT_MODE === 'TESTNET';
