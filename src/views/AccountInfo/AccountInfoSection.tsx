30bb6a17f41e811d301ec3b782318633cc51729b
Sam Newby <samnewby@dydx.exchange>
2025-10-16 11:05:46 -0400

    Moves deposit and withdraw to bottom of account info

--- src/views/AccountInfo/AccountInfoSection.tsx
+++ src/views/AccountInfo/AccountInfoSection.tsx
@@ -2,7 +2,7 @@ import { BonsaiCore } from '@/bonsai/ontology';
 import BigNumber from 'bignumber.js';
 import styled, { css } from 'styled-components';
 
-import { ButtonAction, ButtonShape, ButtonSize, ButtonStyle } from '@/constants/buttons';
+import { ButtonAction, ButtonShape, ButtonSize } from '@/constants/buttons';
 import { ComplianceStates } from '@/constants/compliance';
 import { DialogTypes } from '@/constants/dialogs';
 import { STRING_KEYS } from '@/constants/localization';
@@ -19,7 +19,6 @@ import { Button } from '@/components/Button';
 import { Details } from '@/components/Details';
 import { MarginUsageRing } from '@/components/MarginUsageRing';
 import { OutputType } from '@/components/Output';
-import { WithSeparators } from '@/components/Separator';
 import { WithTooltip } from '@/components/WithTooltip';
 
 import { calculateIsAccountLoading } from '@/state/accountCalculators';
@@ -72,38 +71,35 @@ export const AccountInfoSection = () => {
   const isPostOrderBalanceNegative =
     availableBalancePost != null && MustBigNumber(availableBalancePost).lt(0);
 
-  const withdrawButton = (
-    <$Button
-      state={{ isDisabled: !dydxAccounts }}
-      onClick={() => dispatch(openDialog(DialogTypes.Withdraw2({})))}
-      shape={ButtonShape.Rectangle}
-      size={ButtonSize.XSmall}
-      buttonStyle={ButtonStyle.WithoutBackground}
-      action={ButtonAction.Primary}
-    >
-      {stringGetter({ key: STRING_KEYS.WITHDRAW })}
-    </$Button>
-  );
-
   const depositButton = (
-    <$Button
+    <Button
+      tw="flex-1"
       state={{ isDisabled: !dydxAccounts }}
       onClick={() => dispatch(openDialog(DialogTypes.Deposit2({})))}
       shape={ButtonShape.Rectangle}
       size={ButtonSize.XSmall}
-      buttonStyle={ButtonStyle.WithoutBackground}
       action={ButtonAction.Primary}
     >
       {stringGetter({ key: STRING_KEYS.DEPOSIT })}
-    </$Button>
+    </Button>
+  );
+
+  const withdrawButton = (
+    <Button
+      state={{ isDisabled: !dydxAccounts }}
+      onClick={() => dispatch(openDialog(DialogTypes.Withdraw2({})))}
+      shape={ButtonShape.Rectangle}
+      size={ButtonSize.XSmall}
+      action={ButtonAction.Primary}
+    >
+      {stringGetter({ key: STRING_KEYS.WITHDRAW })}
+    </Button>
   );
 
   const depositWithdrawRow = (
-    <div tw="inlineRow gap-0.5 self-stretch">
-      <$WithSeparators layout="row" withSeparators>
-        {complianceState === ComplianceStates.FULL_ACCESS && depositButton}
-        {withdrawButton}
-      </$WithSeparators>
+    <div tw="inlineRow gap-0.5 self-stretch px-1">
+      {complianceState === ComplianceStates.FULL_ACCESS && depositButton}
+      {withdrawButton}
     </div>
   );
 
@@ -174,13 +170,10 @@ export const AccountInfoSection = () => {
 
   return (
     <$Container>
-      <header tw="spacedRow px-1 py-0 font-small-book">
-        <span>{stringGetter({ key: STRING_KEYS.YOUR_ACCOUNT })}</span>
-        {depositWithdrawRow}
-      </header>
       <$StackContainer $isTablet={isTablet}>
         <$Details items={detailItems} layout="column" withOverflow={false} isLoading={isLoading} />
       </$StackContainer>
+      {depositWithdrawRow}
     </$Container>
   );
 };
@@ -216,16 +209,4 @@ const $StackContainer = styled.div<{ $isTablet: boolean }>`
     css`
       flex: 1;
     `}
-`;
-
-const $WithSeparators = styled(WithSeparators)`
-  --separatorHeight-padding: 0.5rem;
-`;
-
-const $Button = styled(Button)`
-  --button-padding: 0;
-
-  &:hover {
-    text-decoration: underline;
-  }
 `;
