e6ab08d10d4f98f51470365e2e936f73308f723c
lukasz-glen <lukasz.glen@poczta.fm>
2025-10-29 14:33:58 +0100

    feat(indexer): indexer update

---
+++ indexer/src/model/generated/index.ts
@@ -0,0 +1,7 @@
+export * from "./price.model"
+export * from "./liquidity.model"
+export * from "./totalLiquidity.model"
+export * from "./positionKey.model"
+export * from "./position.model"
+export * from "./_positionChange"
+export * from "./totalPosition.model"
---
+++ indexer/test/node.ts
@@ -0,0 +1,19 @@
+import { WalletUnlocked } from "fuels"
+import { DeployContractConfig, launchTestNode, LaunchTestNodeReturn } from "fuels/test-utils"
+
+export async function launchNode(): Promise<LaunchTestNodeReturn<DeployContractConfig[]>> {
+    const launched: LaunchTestNodeReturn<DeployContractConfig[]> = await launchTestNode({
+        walletsConfig: {
+            count: 5, // Number of wallets you want
+        },
+    })
+    return launched
+}
+
+export function getNodeWallets(launchedNode: LaunchTestNodeReturn<DeployContractConfig[]>): WalletUnlocked[] {
+    const {
+        wallets: [deployer, user0, user1, user2, user3],
+    } = launchedNode
+
+    return [deployer, user0, user1, user2, user3]
+}
