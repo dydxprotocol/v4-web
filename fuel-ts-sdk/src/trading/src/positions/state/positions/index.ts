7331fdd565c354b98053dca95d535e504c79e2ae
Kacper Żmijak <kzmijak@gmail.com>
2025-12-30 09:38:08 +0100

    refactor(api): remove positions state layer - migrate to infrastructure/redux
    - Delete state/positions/ (actions, selectors, slice, thunks, types)
    - Delete state/reducer.ts and state/selectors.ts
    - Preparation for new infrastructure/redux pattern

--- fuel-ts-sdk/src/trading/src/positions/state/positions/index.ts
+++
@@ -1,5 +0,0 @@
-export * as actions from './positions.actions';
-export * as selectors from './positions.selectors';
-export * as slice from './positions.slice';
-export * as thunks from './positions.thunks';
-export * as types from './positions.types';
--- fuel-ts-sdk/src/trading/src/positions/state/reducer.ts
+++
@@ -1,4 +0,0 @@
-import { slice, thunks } from './positions';
-
-export const { positionsReducer } = slice;
-export type PositionsThunkExtra = thunks.PositionsThunkExtra;
