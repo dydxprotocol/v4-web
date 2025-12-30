f08327bcd662b8b7fcc5e2034697888fad6df80a
Kacper Żmijak <kzmijak@gmail.com>
2025-12-30 09:38:00 +0100

    refactor(api): remove markets state layer - migrate to infrastructure/redux
    - Delete state/asset-prices/ (selectors, slice, thunks, types)
    - Delete state/candles/ (selectors, slice, thunks, types)
    - Delete state/market-configs/ (actions, selectors, slice, thunks, types)
    - Delete state/reducer.ts and state/selectors.ts
    - Preparation for new infrastructure/redux pattern

--- fuel-ts-sdk/src/trading/src/markets/state/candles/index.ts
+++
@@ -1,4 +0,0 @@
-export * as selectors from './candles.selectors';
-export * as slice from './candles.slice';
-export * as thunks from './candles.thunks';
-export * as types from './candles.types';
--- fuel-ts-sdk/src/trading/src/markets/state/selectors.ts
+++
@@ -1,3 +0,0 @@
-export * from './asset-prices/asset-prices.selectors';
-export * from './candles/candles.selectors';
-export * from './market-configs/market-configs.selectors';
