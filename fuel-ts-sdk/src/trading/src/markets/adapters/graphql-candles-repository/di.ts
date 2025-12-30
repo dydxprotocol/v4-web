75c9c0defc001473ee4e6291169e1c3785d21778
Kacper Żmijak <kzmijak@gmail.com>
2025-12-30 09:38:15 +0100

    refactor(api): remove markets adapters - migrate to infrastructure/repositories
    - Delete adapters/graphql-asset-prices-repository/
    - Delete adapters/graphql-candles-repository/
    - Delete adapters/graphql-market-configs-repository/
    - Delete adapters/index.ts
    - Preparation for new infrastructure/repositories pattern

--- fuel-ts-sdk/src/trading/src/markets/adapters/graphql-candles-repository/di.ts
+++
@@ -1,7 +0,0 @@
-import type { GraphQLClient } from 'graphql-request';
-import { CandleRepository } from '../../domain';
-import { getCandles } from './operations/get-candles';
-
-export const createGraphQLCandleRepository = (client: GraphQLClient): CandleRepository => ({
-  getCandles: getCandles(client),
-});
--- fuel-ts-sdk/src/trading/src/markets/adapters/index.ts
+++
@@ -1,3 +0,0 @@
-export { createGraphQLAssetPriceRepository } from './graphql-asset-prices-repository/di';
-export { createGraphQLCandleRepository } from './graphql-candles-repository/di';
-export { createGraphQLMarketConfigRepository } from './graphql-market-configs-repository/di';
