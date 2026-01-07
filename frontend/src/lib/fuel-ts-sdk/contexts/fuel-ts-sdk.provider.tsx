911a98716bc4ade584201ab1a2657e0537839731
Kacper Żmijak <kzmijak@gmail.com>
2026-01-07 15:04:44 +0100

    chore: update fuel ts sdk provider to use network switch from wallet

--- frontend/src/lib/fuel-ts-sdk/contexts/fuel-ts-sdk.provider.tsx
+++ frontend/src/lib/fuel-ts-sdk/contexts/fuel-ts-sdk.provider.tsx
@@ -1,28 +1,42 @@
 /* eslint-disable react-hooks/refs */
 import { type PropsWithChildren, useRef } from 'react';
 import { createStarboardClient } from 'fuel-ts-sdk/client';
+import type { Asset } from 'fuel-ts-sdk/trading';
 import { Provider as ReduxProvider } from 'react-redux';
+import localAssets from '@/assets/local-assets.json';
+import testnetAssets from '@/assets/testnet-assets.json';
+import { NetworkSwitchContext } from '@/contexts/network-switch/network-switch.context';
+import { getIndexerUrl } from '@/lib/env';
+import { useRequiredContext } from '@/lib/use-required-context.hook';
+import type { Network } from '@/models/network';
 import type { FuelTsSdkContextType } from './fuel-ts-sdk.context';
 import { FuelTsSdkContext } from './fuel-ts-sdk.context';
 
-interface FuelTsSdkProviderProps extends PropsWithChildren {
-  indexerUrl: string;
-}
+interface FuelTsSdkProviderProps extends PropsWithChildren {}
 
-export function FuelTsSdkProvider({ children, indexerUrl }: FuelTsSdkProviderProps) {
+export function FuelTsSdkProvider({ children }: FuelTsSdkProviderProps) {
+  const currentNetwork = useRequiredContext(NetworkSwitchContext).getCurrentNetwork();
+  const indexerUrl = getIndexerUrl(currentNetwork);
+  const assets = getNetworkAssets(currentNetwork);
+
   const clientRef = useRef<FuelTsSdkContextType | null>(null);
 
   if (clientRef.current == null) {
     clientRef.current = createStarboardClient({
-      indexerUrl: indexerUrl,
+      indexerUrl,
     });
+    clientRef.current.trading.populateAssets(assets);
   }
 
-  const client = clientRef.current;
-
   return (
-    <ReduxProvider store={client.store}>
-      <FuelTsSdkContext.Provider value={client}>{children}</FuelTsSdkContext.Provider>
+    <ReduxProvider store={clientRef.current.store}>
+      <FuelTsSdkContext.Provider value={clientRef.current}>{children}</FuelTsSdkContext.Provider>
     </ReduxProvider>
   );
+}
+
+function getNetworkAssets(network: Network) {
+  if (network === 'local') return localAssets as Asset[];
+  if (network === 'testnet') return testnetAssets as Asset[];
+  throw new Error(`Unsupported Network:  ${network}`);
 }
