4d442c7fb4065cef01f6cc1a8e983e8b13fc15cc
lukasz-glen <lukasz.glen@poczta.fm>
2026-01-28 00:13:12 +0100

    fix(indexer): add open id to the current positions

--- indexer/db/migrations/1768329135950-Data.js
+++ indexer/db/migrations/1768329135950-Data.js
@@ -3,7 +3,7 @@ export default class Data1768329135950 {
   name = 'Data1768329135950';
 
   async up(db) {
-    await db.query(`CREATE TABLE "current_position" ("position_id" character varying NOT NULL, "change" character varying(9) NOT NULL, "timestamp" integer NOT NULL, "collateral" numeric NOT NULL, "size" numeric NOT NULL, "out_average_price" numeric NOT NULL, "position_key_id" character varying, CONSTRAINT "PK_CURRENT_POSITION" PRIMARY KEY ("position_key_id"))`);
+    await db.query(`CREATE TABLE "current_position" ("position_id" character varying NOT NULL, "change" character varying(9) NOT NULL, "timestamp" integer NOT NULL, "collateral" numeric NOT NULL, "size" numeric NOT NULL, "out_average_price" numeric NOT NULL, "position_open_id" character varying NOT NULL UNIQUE, "position_key_id" character varying, CONSTRAINT "PK_CURRENT_POSITION" PRIMARY KEY ("position_key_id"))`);
     await db.query(`
       CREATE OR REPLACE FUNCTION current_position_update() RETURNS TRIGGER AS $$
       BEGIN
@@ -11,7 +11,7 @@ export default class Data1768329135950 {
             IF NEW.change = 'CLOSE' OR NEW.change = 'LIQUIDATE' OR NEW.size = 0 THEN
                DELETE FROM current_position WHERE position_key_id = NEW.position_key_id;
             ELSE
-               INSERT INTO current_position VALUES(NEW.id, NEW.change, NEW.timestamp, NEW.collateral, NEW.size, NEW.out_average_price, NEW.position_key_id)
+               INSERT INTO current_position VALUES(NEW.id, NEW.change, NEW.timestamp, NEW.collateral, NEW.size, NEW.out_average_price, NEW.open_id, NEW.position_key_id)
                   ON CONFLICT(position_key_id)
                   DO UPDATE SET position_id = NEW.id, change = NEW.change, timestamp = NEW.timestamp, collateral = NEW.collateral, size = NEW.size, out_average_price = NEW.out_average_price;
             END IF;
