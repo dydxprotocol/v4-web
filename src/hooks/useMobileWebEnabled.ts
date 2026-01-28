6823512b3bef9e3a874c0a73177ef5c2d8fa81f0
KevDydx <kevin@dydx.exchange>
2026-01-28 11:16:04 -0500

    comment

--- src/hooks/useMobileWebEnabled.ts
+++ src/hooks/useMobileWebEnabled.ts
@@ -4,6 +4,7 @@ import { testFlags } from '@/lib/testFlags';
 
 import { useStatsigGateValue } from './useStatsig';
 
+// Flag for determining whether we show the new mobile web or not. Disables simpleui if enabled
 export const useMobileWebEnabled = () => {
   const forceMobileWeb = testFlags.enableMobileWeb;
   const mobileWebbFF = useStatsigGateValue(StatsigFlags.ffMobileWeb);
