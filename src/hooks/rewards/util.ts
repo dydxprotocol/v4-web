bcb36bf82a36513b195ac766226a0b8d8c1537de
Luka Buzaladze <lbuzaladze99@gmail.com>
2026-02-09 12:28:36 -0500

    fix: fix bonk rewards calculation (#41)

--- src/hooks/rewards/util.ts
+++ src/hooks/rewards/util.ts
@@ -29,8 +29,8 @@ export function positionToBonkRewards(position: number | undefined) {
   if (position === 2) return 15000;
   if (position === 3) return 10000;
   if (position === 4 || position === 5) return 5000;
-  if (position >= 6 || position <= 10) return 4000;
-  if (position >= 11 || position <= 20) return 2000;
+  if (position >= 6 && position <= 10) return 4000;
+  if (position >= 11 && position <= 20) return 2000;
   return 0;
 }
 
